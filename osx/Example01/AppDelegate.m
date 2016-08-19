0eebfed75e38c29bf3bba0de2587772d9606dfe7
evan <evan-github@madstyle.net>
2016-08-19 00:31:39 +0000

    Added Example01 to osx.  Probably not the final resting place, but it's showing signs of life, so it's time to save it.

---
+++ osx/Example01/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Example01
+//
+//  Created by Evan Mortimore on 8/18/16.
+//
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ osx/Example01/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Example01
+//
+//  Created by Evan Mortimore on 8/18/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    NSLog(@"in main()");
+    return NSApplicationMain(argc, argv);
+}
