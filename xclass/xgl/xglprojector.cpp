50b7ca0296ad45a5afb040d2e0d772ba3c3cbf43
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-08-24 21:14:57 +0000

    Made sure EVERY glXxx() is followed by a GL_CHECK() call.

--- xclass/xgl/xglprojector.cpp
+++ xclass/xgl/xglprojector.cpp
@@ -7,8 +7,10 @@ void XGLProjector::Reshape(int w, int h)
 	width = w;
 	height = h;
     glViewport(0, 0, width, height);
+	GL_CHECK("glViewport() failed");
 	smd->projection = glm::perspective(glm::radians(45.0f), float(width) / float(height), 0.1f, 1000.0f);
 
 	glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(smd->projection), (GLvoid *)&smd->view);
+	GL_CHECK("glBufferSubData() failed");
 }
 
