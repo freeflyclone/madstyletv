f017294380bce72b6b46921201a83221d1270a5c
evan <evan-github@madstyle.net>
2016-08-24 23:44:30 +0000

    Made all the toruses shiny.

--- examples/Example02/Example02.cpp
+++ examples/Example02/Example02.cpp
@@ -12,16 +12,16 @@ void ExampleXGL::BuildScene() {
 	shape->SetColor(yellow);
 
 
-	AddShape("shaders/diffuse", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
+	AddShape("shaders/lighting", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
 	shape->SetColor(blue);
 	shape->model = glm::translate(glm::mat4(), glm::vec3(10, 0, 0));
 
-	AddShape("shaders/diffuse", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
+	AddShape("shaders/lighting", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
 	shape->SetColor(red);
 	shape->model = glm::translate(glm::mat4(), glm::vec3(-20, 0, 0)) 
 				 * glm::scale(glm::mat4(), glm::vec3(2, 2, 2));
 
-	AddShape("shaders/diffuse", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
+	AddShape("shaders/lighting", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
 	shape->SetColor(green);
 	shape->model = glm::translate(glm::mat4(), glm::vec3(20, 0, 0)) 
 				 * glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(0,1,0)) 
