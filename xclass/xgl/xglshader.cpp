c75c2c395a0079937527bc234bafb2faed03f26b
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-08-25 19:23:51 +0000

    Beginnings of XGLShader refactoring:  I've not been satisfied with what I did originally, and I have a better idea of what's needed and what is more elegant.

--- xclass/xgl/xglshader.cpp
+++ xclass/xgl/xglshader.cpp
@@ -65,7 +65,7 @@ void XGLShaderComponent::InfoLog(std::string fileName) {
 	}
 }
 
-XGLShader::XGLShader() : shader(-1) {} 
+XGLShader::XGLShader() : programId(-1) {} 
 XGLShader::~XGLShader() { }
 
 bool XGLShader::Compile(std::string name) {
@@ -76,81 +76,81 @@ bool XGLShader::Compile(std::string name) {
     if (!mFShader.Compile(name, GL_FRAGMENT_SHADER))
         throwXGLException("Compiling fragment shader failed.");
 
-    shader = glCreateProgram();
+	programId = glCreateProgram();
     GL_CHECK("glCreateProgram() failed");
 
-	glAttachShader(shader, mVShader.mShader);
+	glAttachShader(programId, mVShader.mShader);
     GL_CHECK("glAttachShader(VERTEX) failed");
-    glAttachShader(shader, mFShader.mShader);
+	glAttachShader(programId, mFShader.mShader);
     GL_CHECK("glAttachShader(FRAGMENT) failed");
-    glBindAttribLocation(shader, 0, "in_Position");
+	glBindAttribLocation(programId, 0, "in_Position");
     GL_CHECK("glBindAttribLocation(\"in_Position\") failed");
-	glBindAttribLocation(shader, 1, "in_TexCoord");
+	glBindAttribLocation(programId, 1, "in_TexCoord");
 	GL_CHECK("glBindAttribLocation(\"in_TexCoord\") failed");
-	glBindAttribLocation(shader, 2, "in_Normal");
+	glBindAttribLocation(programId, 2, "in_Normal");
     GL_CHECK("glBindAttribLocation(\"in_Normal\") failed");
-    glBindAttribLocation(shader, 3, "in_Color");
+	glBindAttribLocation(programId, 3, "in_Color");
     GL_CHECK("glBindAttribLocation(\"in_Color\") failed");
 
-	glLinkProgram(shader);
+	glLinkProgram(programId);
     GL_CHECK("glLinkProgram() failed");
-    glValidateProgram(shader);
+	glValidateProgram(programId);
     GL_CHECK("glValidateProgram() failed");
     
-    glGetProgramiv(shader, GL_LINK_STATUS, &status);
+	glGetProgramiv(programId, GL_LINK_STATUS, &status);
     GL_CHECK("glGetProgramiv() failed");
     if (status != GL_TRUE)
         GL_CHECK("compile failed in the link stage");
-    glGetProgramiv(shader, GL_VALIDATE_STATUS, &status);
+	glGetProgramiv(programId, GL_VALIDATE_STATUS, &status);
     GL_CHECK("glGetProgramiv() failed");
-    glGetProgramiv(shader, GL_ATTACHED_SHADERS, &status);
+	glGetProgramiv(programId, GL_ATTACHED_SHADERS, &status);
     GL_CHECK("glGetProgramiv() failed");
-    glGetProgramiv(shader, GL_INFO_LOG_LENGTH, &status);
+	glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &status);
     GL_CHECK("glGetProgramiv() failed");
 
-	GLuint idx = glGetUniformBlockIndex(shader, "ShaderMatrixData");
+	GLuint idx = glGetUniformBlockIndex(programId, "ShaderMatrixData");
 	GL_CHECK("glGetUniformBlockIndex() failed");
 	if (idx == GL_INVALID_INDEX) {
 		xprintf("ShaderMatrixData not found in '%s'\n", name.c_str());
 	}
 	else {
 		// assign to uniform block 0
-		glUniformBlockBinding(shader, idx, 0);
+		glUniformBlockBinding(programId, idx, 0);
 		GL_CHECK("glUniformBlockBinding() failed");
 	}
 
-	idx = glGetUniformBlockIndex(shader, "LightData");
+	idx = glGetUniformBlockIndex(programId, "LightData");
 	GL_CHECK("glGetUniformBlockIndex() failed");
 	if (idx == GL_INVALID_INDEX) {
 		xprintf("LightData not found in '%s'\n", name.c_str());
 	}
 	else {
 		// assign to uniform block 1
-		glUniformBlockBinding(shader, idx, 1);
+		glUniformBlockBinding(programId, idx, 1);
 		GL_CHECK("glUniformBlockBinding() failed");
 	}
 
-	glUseProgram(shader);
+	glUseProgram(programId);
 
 	XGLLights lights = XGL::getInstance()->lights;
 	XGLLight light = lights.back();
 
-	glUniform3fv(glGetUniformLocation(shader, "light.position"), 1, (GLfloat*)glm::value_ptr(light.position));
+	glUniform3fv(glGetUniformLocation(programId, "light.position"), 1, (GLfloat*)glm::value_ptr(light.position));
 	GL_STATUS();
 
-	glUniform3fv(glGetUniformLocation(shader, "light.intensities"), 1, (GLfloat*)glm::value_ptr(light.diffuse));
+	glUniform3fv(glGetUniformLocation(programId, "light.intensities"), 1, (GLfloat*)glm::value_ptr(light.diffuse));
 	GL_STATUS();
 
-	glUniform1f(glGetUniformLocation(shader, "light.attenuation"), light.attenuation);
+	glUniform1f(glGetUniformLocation(programId, "light.attenuation"), light.attenuation);
 	GL_STATUS();
 
-	glUniform1f(glGetUniformLocation(shader, "light.ambientCoefficient"), light.ambientCoefficient);
+	glUniform1f(glGetUniformLocation(programId, "light.ambientCoefficient"), light.ambientCoefficient);
 	GL_STATUS();
 
-	glUniform3fv(glGetUniformLocation(shader, "materialSpecularColor"), 1, (GLfloat*)glm::value_ptr(white));
+	glUniform3fv(glGetUniformLocation(programId, "materialSpecularColor"), 1, (GLfloat*)glm::value_ptr(white));
 	GL_STATUS();
 
-	glUniform1f(glGetUniformLocation(shader, "materialShininess"), 100.0f);
+	glUniform1f(glGetUniformLocation(programId, "materialShininess"), 100.0f);
 	GL_STATUS();
 
 	//DebugPrintf("XGLShader::Compile(%s) shader #: %d\n", name.c_str(), shader);
@@ -162,12 +162,12 @@ void XGLShader::InfoLog() {
 	int nWritten;
 	char infoLog[8192];
 
-	glValidateProgram(shader);
+	glValidateProgram(programId);
     GL_CHECK("glValidateProgram() failed");
-	glGetProgramiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+	glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &infoLogLength);
     GL_CHECK("glGetProgramiv() failed");
 	if( infoLogLength ) {
-		glGetProgramInfoLog(shader, infoLogLength, &nWritten, infoLog);
+		glGetProgramInfoLog(programId, infoLogLength, &nWritten, infoLog);
 		//xprintf("%s\n\n", infoLog);
 	}
 }
@@ -176,9 +176,9 @@ void XGLShader::Use() {
     GLint currentProgram;
     glGetIntegerv(GL_CURRENT_PROGRAM,&currentProgram);
     GL_CHECK("glGetIntegerv() failed");
-    if( shader != currentProgram) {
-        glUseProgram(shader);
-        GL_CHECK("glUseProgram(shader) failed");
+	if (programId != currentProgram) {
+		glUseProgram(programId);
+        GL_CHECK("glUseProgram(programId) failed");
     }
 }
 
@@ -189,4 +189,26 @@ void XGLShader::UnUse() {
 
 void XGLShader::Reshape(int w, int h){
 	xprintf("XGLShader::Reshape(%d,%d) for '%s'\n", w, h, shaderName.c_str());
+}
+
+std::string XGLShader::Name() {
+	return shaderName;
+}
+
+GLint XGLShader::Id() {
+	return programId;
+}
+
+GLint XGLShader::Attrib(std::string name) {
+	GLint attrib = glGetAttribLocation(programId, name.c_str());
+	if (attrib == -1)
+		throwXGLException("Vertex attribute not found: " + name);
+	return attrib;
+}
+
+GLint XGLShader::Uniform(std::string name) {
+	GLint uniform = glGetUniformLocation(programId, name.c_str());
+	if (uniform == -1)
+		throwXGLException("Shader uniform not found: " + name);
+	return uniform;
 }
