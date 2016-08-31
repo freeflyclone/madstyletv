b64c105083c465099c94482a252dd1470144946e
evan <evan-github@madstyle.net>
2016-08-31 01:36:04 +0000

    This one doesn't crash!  Not sure if it's any good or not...

--- linux/main.cpp
+++ linux/main.cpp
@@ -22,8 +22,8 @@ int main(void) {
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window) {
