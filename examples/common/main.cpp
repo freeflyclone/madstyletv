2e3e60c59869dada93ec719562e83b2a0dd34cf8
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-09-02 01:20:53 +0000

    adding main.cpp - a GLFW file copied from the linux part of the tree, with slight modifications for Windows.

---
+++ examples/common/main.cpp
@@ -0,0 +1,127 @@
+// glfw-test.cpp : Defines the entry point for the console application.
+#include <Windows.h>
+#include <stdio.h>
+
+#include <glew.h>
+#include <GLFW/glfw3.h>
+
+#include <xgl.h>
+#include <ExampleXGL.h>
+
+static ExampleXGL *exgl = NULL;
+
+void SetGlobalWorkingDirectoryName()
+{
+	DWORD sizeNeeded = GetCurrentDirectory(0, NULL);
+	DWORD size;
+	TCHAR *buff = new TCHAR[sizeNeeded];
+
+	if ((size = GetCurrentDirectory(sizeNeeded, buff)) != sizeNeeded - 1)
+		throwXGLException("GetCurrentDirectory() unexpectedly failed. " + std::to_string(size) + " vs " + std::to_string(sizeNeeded));
+
+#ifdef UNICODE
+	std::wstring wstr(buff);
+	currentWorkingDir = std::string(wstr.begin(), wstr.end());
+#else
+	currentWorkingDir = std::string(buff);
+#endif
+	delete[] buff;
+
+	// this presumes that a VS Post-Build step copies the result of the build to $(SolutionDir)\bin
+	// it is further presumed that ALL OS specific build procedures will do the same.
+	// (also works if project's Debug property "working directory" is set to $(SolutionDir))
+	// So we got that going for us.
+	pathToAssets = currentWorkingDir.substr(0, currentWorkingDir.rfind("\\bin"));
+}
+
+void error_callback(int error, const char *description) {
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	else {
+		if (exgl != NULL) {
+			if (action == GLFW_PRESS)
+				exgl->KeyEvent(key, 0);
+			else if (action == GLFW_RELEASE)
+				exgl->KeyEvent(key, 0x8000);
+		}
+	}
+}
+
+static void cursor_position_callback(GLFWwindow *window, double x, double y) {
+	int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+	if (state == GLFW_PRESS)
+		exgl->MouseEvent((int)x, (int)y, 1);
+}
+
+int main(void) {
+	GLFWwindow *window;
+
+	if (!FreeConsole()) {
+		xprintf("Freeing the console failed: %d\n", GetLastError());
+		exit(0);
+	}
+
+	if (!glfwInit()) {
+		printf("glfwInit() failed\n");
+		return -1;
+	}
+
+	glfwSetErrorCallback(error_callback);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_SAMPLES, 8);
+
+	window = glfwCreateWindow(1920, 1080, "Mad Style TV Example", NULL, NULL);
+	if (!window) {
+		printf("glfwCreateWindow() failed\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	glfwSwapInterval(1);
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		printf("glewInit() failed: %s\n", glewGetErrorString(err));
+		exit(-1);
+	}
+	
+	SetGlobalWorkingDirectoryName();
+
+	xprintf("current working dir: %s\n", currentWorkingDir.c_str());
+	pathToAssets = currentWorkingDir + "\\..";
+
+	xprintf("pathToAssets: %s\n", pathToAssets.c_str());
+
+	exgl = new ExampleXGL();
+
+	while (!glfwWindowShouldClose(window)) {
+		int width, height;
+
+		glfwGetFramebufferSize(window, &width, &height);
+		exgl->Reshape(width, height);
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		exgl->Display();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
