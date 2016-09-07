c9be9dd18cf657b349fdec3c3b1cad5a1b39add2
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-09-07 21:46:13 +0000

    Adding "tex" shader, which is a simple texture mapping shader, that doesn't do lighting. Added a case in xglbuffer for a 3-component (RGB without alpha) glTexImage2D(). Added scaling, translating and rotating to Example05, because why not.

--- examples/Example05/Example05BuildScene.cpp
+++ examples/Example05/Example05BuildScene.cpp
@@ -2,12 +2,19 @@
 ** Example05BuildScene.cpp
 **
 ** Just to demonstrate instantiation of a "ground"
-** plane and a single texture-mapped quad.
+** plane and a single texture-mapped quad, along with
+** scaling, translating and rotating the quad using the GLM
+** functions.
 **************************************************************/
 #include "ExampleXGL.h"
 
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
-	std::string imgPath = pathToAssets + "/assets/8bit.png";
-	AddShape("shaders/textShader", [&](){ shape = new XGLTexQuad(imgPath); return shape; });
+	std::string imgPath = pathToAssets + "/assets/AndroidDemo.png";
+	AddShape("shaders/tex", [&](){ shape = new XGLTexQuad(imgPath); return shape; });
+
+	glm::mat4 scale = glm::scale(glm::mat4(), glm::vec3(9.6f, 5.4f, 1.0f));
+	glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, 5.4f));
+	glm::mat4 rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	shape->model = translate * rotate * scale;
 }
---
+++ shaders/tex.vert
@@ -0,0 +1,23 @@
+#version 150
+
+layout (std140) shared uniform ShaderMatrixData {
+	mat4 projector;
+	mat4 view;
+	mat4 model;
+};
+
+
+in  vec3 in_Position;
+in  vec2 in_TexCoord;
+in  vec3 in_Normal;
+in  vec3 in_Color;
+
+out vec4 ex_Color;
+out vec2 UV;
+
+void main(void)
+{
+    gl_Position = projector * view * model * vec4(in_Position, 1.0);
+    ex_Color = vec4(in_Color, 1.0);
+    UV = in_TexCoord;
+}
