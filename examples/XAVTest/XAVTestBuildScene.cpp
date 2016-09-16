9204f41ec9e5bc038cfcb1cb22639dc7d268c350
evan <evan-github@madstyle.net>
2016-09-16 18:43:13 +0000

    Framerate progress:
    Fixing the bug in XAVSrc::GetBuffer() results in expected behavior there.
    
    However, my XFifo for ImageBuff in XAVTestBuildScene causes a stack overflow still.  This version doesn't fix THAT, but it does rip through the video file at full speed now.

--- examples/XAVTest/XAVTestBuildScene.cpp
+++ examples/XAVTest/XAVTestBuildScene.cpp
@@ -5,17 +5,36 @@
 #include "ExampleXGL.h"
 
 #include <xavfile.h>
+#include <xfifo.h>
 
-class VideoFileThread : public XGLObject {
+typedef struct {
+	unsigned char b[960 * 810];
+} ImageBuff;
+
+ImageBuff ib;
+
+class VideoFileThread : public XGLObject , public XThread {
 public:
-	VideoFileThread(std::string url) : XGLObject("VideoFileThread") {
+	VideoFileThread(std::string url) : XGLObject("VideoFileThread"), XThread("VideoFIleThread"), imageFifo(4) {
 		// first thing MUST be av_register_all()
 		av_register_all();
 		xavFile = new XAVFile(url);
 		xavFile->Start();
 	}
 
+	void Run() {
+		while (IsRunning()) {
+			unsigned char *image;
+			image = xavFile->mVideoStream->GetBuffer();
+			memcpy(&imageBuff.b, image, sizeof(imageBuff));
+			ib = imageBuff;
+			//imageFifo.Put(imageBuff);
+		}
+	}
+
 	XAVFile *xavFile;
+	XFifo<ImageBuff> imageFifo;
+	ImageBuff imageBuff;
 };
 
 VideoFileThread *pvft;
@@ -33,15 +52,18 @@ void ExampleXGL::BuildScene() {
 	glm::mat4 rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	shape->model = translate * rotate * scale;
 
-	pvft = new VideoFileThread(videoPath);
-
 	XGLShape::AnimaFunk transform = [&](XGLShape *s, float clock) {
-		s->b.Bind();
-		unsigned char *image;
-		image = pvft->xavFile->mVideoStream->GetBuffer();
+		if (pvft != NULL && pvft->IsRunning()) {
+			s->b.Bind();
+			//ib = pvft->imageFifo.Get();
+			unsigned char *image = ib.b;
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 960, 540, 0, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)image);
-		GL_CHECK("glGetTexImage() didn't work");
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, 960, 540, 0, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)image);
+			GL_CHECK("glGetTexImage() didn't work");
+		}
 	};
 	shape->SetTheFunk(transform);
+
+	pvft = new VideoFileThread(videoPath);
+	pvft->Start();
 }
