d1d605e1a54e174e727a2f030fba5ce7df419532
evan <evan-github@madstyle.net>
2016-09-22 20:31:52 +0000

    Added a rotating torus, for eye candy.

--- examples/XAVTest/XAVTestBuildScene.cpp
+++ examples/XAVTest/XAVTestBuildScene.cpp
@@ -219,10 +219,19 @@ void ExampleXGL::BuildScene() {
 	std::string imgPath = pathToAssets + "/assets/AndroidDemo.png";
 	std::string videoPath = pathToAssets + "/assets/CulturalPhenomenon.mp4";
 
-	AddShape("shaders/yuv", [&](){ shape = new XGLTexQuad(imgPath); return shape; });
+	AddShape("shaders/lighting", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
+	shape->SetColor(yellow);
+	shape->SetTheFunk([&](XGLShape *s, float clock) {
+		float translateFunction = sin(clock / 60.0f);
+		glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(translateFunction*4.0f, 0.0f, 0.0f));
+		glm::mat4 rotate = glm::rotate(glm::mat4(), clock / 40.0f, glm::vec3(1.0f, 0.0f, 0.0f));
 
+		s->model = translate * rotate;
+	});
+
+	AddShape("shaders/yuv", [&](){ shape = new XGLTexQuad(imgPath); return shape; });
 	glm::mat4 scale = glm::scale(glm::mat4(), glm::vec3(9.6f, 5.4f, 1.0f));
-	glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0, 0, 5.4f));
+	glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0, 4.0f, 5.4f));
 	glm::mat4 rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	shape->model = translate * rotate * scale;
 
