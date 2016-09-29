c53bec3cb7b94b4c24c60e8ac334c2701e53535d
evan <evan-github@madstyle.net>
2016-09-29 23:24:26 +0000

    Adding XAL to the project folder structure.  This is the XGL equivalent for OpenAL.

---
+++ xclass/xal/xal.h
@@ -0,0 +1,27 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 Evan Mortimore
+** All rights reserved.
+**
+****************************************************************************/
+#ifndef XAL_H
+#define XAL_H
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <al.h>
+#include <alc.h>
+#include <alext.h>
+
+#include "xutils.h"
+
+class XAL {
+public:
+	XAL();
+	virtual ~XAL();
+};
+
+#endif
