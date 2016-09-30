a2bee08d8349e6e42704336aca95e93d1e007f15
evan <evan-github@madstyle.net>
2016-09-30 02:08:18 +0000

    Moved all OpenAL code out of OpenALTestBuildScene and into xal.cpp There is no threading yet in XAL, and that's probably the next step for XAV goodness.

--- xclass/xal/xal.h
+++ xclass/xal/xal.h
@@ -12,15 +12,39 @@
 #include <memory>
 #include <sstream>
 #include <stdexcept>
+
+// OpenAL includes
 #include <al.h>
 #include <alc.h>
 
 #include "xutils.h"
 
+typedef std::vector<std::string> XALDeviceList;
+
 class XAL {
 public:
 	XAL();
 	virtual ~XAL();
+
+	void Play();
+	void Pause();
+	void Stop();
+
+	void TestTone();
+
+	XALDeviceList EnumerateDevices();
+
+private:
+	XALDeviceList deviceList;
+
+	ALCchar *deviceName;
+	ALCdevice *audioDevice;
+	ALCcontext *audioContext;
+	ALuint alBufferId;
+	ALuint alSourceId;
+	ALenum alError;
+
+	short testToneBuffer[48000 * 4];
 };
 
 #endif
