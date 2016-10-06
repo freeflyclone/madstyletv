ddb6b9ae6d5761082c892d4f488f4bffbc5d66a1
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-10-06 03:12:19 +0000

    Added XAssets as direct sub-class of JSONValue.  XConfig is now a sub-class of XAssets, all it does it specify name for the config file in assets/config.json

--- xclass/xconfig.h
+++ xclass/xconfig.h
@@ -3,38 +3,18 @@
 ** Copyright (C) 2016 Evan Mortimore
 ** All rights reserved.
 **
-** definitions of XConfig objects:
-**
+** XConfig used to be XML based, but it's been switched to XAssets.
+** 
+** See xassets.h for further details.
 ****************************************************************************/
 #ifndef XCONFIG_H
 #define XCONFIG_H
 
-#include <list>
-#include <expat.h>
+#include <xassets.h>
 
-typedef std::pair<std::string, std::string> XConfigKeyValuePair;
-
-class XConfig {
+class XConfig : public XAssets {
 public:
-    XConfig();
-    ~XConfig();
-
-private:
-    void Start(const char *name, const char *args[]);
-    void Value(const char *val, int len);
-    void End(const char *name);
-
-    static void start(void *userData, const char *name, const char *args[]);
-    static void value(void *userData, const char *val, int len);
-    static void end(void *userData, const char *name);
-
-    XML_Parser parser;
-
-    std::vector<XConfigKeyValuePair> nodes;
-    std::vector<std::string> nodeStack;
-
-    int level;
-    int numLeaves;
+	XConfig(std::string fileName = std::getenv("XCLASS_DIR") + std::string("/../assets/config.json")) : XAssets(fileName) {};
 };
 
 #endif
