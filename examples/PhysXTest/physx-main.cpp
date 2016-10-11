9aee6bdf29659493c6680c871e8f5b0425ef97dc
evan <evan-github@madstyle.net>
2016-10-11 19:40:17 +0000

    First version that seems to work somewhat.  Only in Debug configuration though.  Tracking that down now.

---
+++ examples/PhysXTest/physx-main.cpp
@@ -0,0 +1,141 @@
+// physx-main.cpp : Defines the entry point for the GLFW-based PhysX application.
+// The console window is immediately closed.
+#include <Windows.h>
+#include <stdio.h>
+
+#include <glew.h>
+#include <GLFW/glfw3.h>
+
+#include <xgl.h>
+#include <physx-xgl.h>
+
+static PhysXXGL *pxgl = NULL;
+
+void SetGlobalWorkingDirectoryName()
+{
+	DWORD sizeNeeded = GetCurrentDirectory(0, NULL);
+	DWORD size;
+	TCHAR *buff = new TCHAR[sizeNeeded];
+
+	if ((size = GetCurrentDirectory(sizeNeeded, buff)) != sizeNeeded - 1)
+		throwXGLException("GetCurrentDirectory() unexpectedly failed. " + std::to_string(size) + " vs " + std::to_string(sizeNeeded));
+
+#ifdef UNICODE
+	std::wstring wstr(buff);
+	currentWorkingDir = std::string(wstr.begin(), wstr.end());
+#else
+	currentWorkingDir = std::string(buff);
+#endif
+	delete[] buff;
+
+	// this presumes that a VS Post-Build step copies the result of the build to $(SolutionDir)\bin
+	// it is further presumed that ALL OS specific build procedures will do the same.
+	// (also works if project's Debug property "working directory" is set to $(SolutionDir))
+	// So we got that going for us.
+	pathToAssets = currentWorkingDir.substr(0, currentWorkingDir.rfind("\\bin"));
+}
+
+void error_callback(int error, const char *description) {
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	else {
+		if (pxgl != NULL) {
+			if (action == GLFW_PRESS)
+				pxgl->KeyEvent(key, 0);
+			else if (action == GLFW_RELEASE)
+				pxgl->KeyEvent(key, 0x8000);
+		}
+	}
+}
+
+static void cursor_position_callback(GLFWwindow *window, double x, double y) {
+	int state = 0;
+
+	state |= glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+	state |= glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) << 1;
+
+	if (pxgl != NULL)
+		pxgl->MouseEvent((int)x, (int)y, state);
+}
+
+int main(void) {
+	GLFWwindow *window;
+	/*
+	if (!FreeConsole()) {
+	printf("Freeing the console failed: %d\n", GetLastError());
+	exit(0);
+	}
+	*/
+	if (!glfwInit()) {
+		printf("glfwInit() failed\n");
+		return -1;
+	}
+
+	glfwSetErrorCallback(error_callback);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_SAMPLES, 8);
+
+	window = glfwCreateWindow(1280, 720, "Mad Style TV Example", NULL, NULL);
+	if (!window) {
+		printf("glfwCreateWindow() failed\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	glfwSwapInterval(1);
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		printf("glewInit() failed: %s\n", glewGetErrorString(err));
+		exit(-1);
+	}
+
+	SetGlobalWorkingDirectoryName();
+
+	xprintf("current working dir: %s\n", currentWorkingDir.c_str());
+	pathToAssets = currentWorkingDir + "\\..";
+
+	xprintf("pathToAssets: %s\n", pathToAssets.c_str());
+
+	try {
+		pxgl = new PhysXXGL();
+		xprintf("About to call initPhysics()\n");
+		pxgl->initPhysics(true);
+
+		while (!glfwWindowShouldClose(window)) {
+			int width, height;
+
+			glfwGetFramebufferSize(window, &width, &height);
+			pxgl->Reshape(width, height);
+
+			glfwSwapInterval(1);
+
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			pxgl->Display();
+
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+
+		glfwTerminate();
+	}
+	catch (std::runtime_error e) {
+		printf("Exception: %s\n", e.what());
+	}
+
+	return 0;
+}
---
+++ examples/PhysXTest/physx-xgl.h
@@ -0,0 +1,106 @@
+/****************************************************************************
+** Copyright (C) 2016 Evan Mortimore
+** All rights reserved. (for now)
+**
+** Description:
+**	Generic application wrapper for xclass, XGL, and XInput application
+**  example, with PhysX integration.
+*****************************************************************************/
+#pragma once
+
+#include <ExampleXGL.h>
+#include <pxphysicsapi.h>
+
+#define MAX_NUM_ACTOR_SHAPES 128
+
+// define a bindable function pointer for various Physx Joint types creation
+typedef std::function<physx::PxJoint *(physx::PxRigidActor *, const physx::PxTransform&, physx::PxRigidActor*, const physx::PxTransform&)> MstvPhysxCreateJointFunk;
+
+class PhysXXGL : public ExampleXGL, public physx::PxUserControllerHitReport {
+public:
+	PhysXXGL();
+
+	physx::PxDefaultAllocator			mAllocator;
+	physx::PxDefaultErrorCallback		mErrorCallback;
+	physx::PxFoundation*				mFoundation;
+	physx::PxPhysics*					mPhysics;
+	physx::PxDefaultCpuDispatcher*		mDispatcher;
+	physx::PxScene*						mScene;
+	physx::PxMaterial*					mMaterial;
+	physx::PxVisualDebuggerConnection*	mConnection;
+	physx::PxControllerManager*			mControllerManager;
+	physx::PxController*				mController;
+
+	physx::PxReal						stackY = 10.f;
+	physx::PxCooking*					gCooking;
+
+	physx::PxRigidDynamic *createDynamic(const physx::PxTransform& t, const physx::PxGeometry& g, const physx::PxVec3& velocity = physx::PxVec3(0));
+	physx::PxRigidDynamic *createDynamic(const physx::PxTransform &, const physx::PxGeometry &, physx::PxMaterial &, float);
+
+	void createStack(const physx::PxTransform& t, physx::PxU32 size, physx::PxReal halfExtent);
+
+	// define functions for creating various types of Physx Joints
+	physx::PxJoint* createLimitedSpherical(physx::PxRigidActor*, const physx::PxTransform&, physx::PxRigidActor*, const physx::PxTransform&);
+	physx::PxJoint* createBreakableFixed(physx::PxRigidActor*, const physx::PxTransform&, physx::PxRigidActor*, const physx::PxTransform&);
+	physx::PxJoint* createDampedD6(physx::PxRigidActor*, const physx::PxTransform&, physx::PxRigidActor*, const physx::PxTransform&);
+
+	// create a chain of Physx Joints of various styles.  This could probably be expanded
+	void createChain(const physx::PxTransform&, physx::PxU32, const physx::PxGeometry&, physx::PxReal, MstvPhysxCreateJointFunk);
+
+	void initPhysics(bool interactive);
+	void stepPhysics(bool interactive);
+	void cleanupPhysics(bool interactive);
+	void renderGeometry(physx::PxGeometryHolder, physx::PxMat44, bool);
+
+	void RenderActors(physx::PxRigidActor** actors, const physx::PxU32 numActors, bool shadows = false, const physx::PxVec3 & color = physx::PxVec3(0.0f, 0.75f, 0.0f));
+
+	void ShapeToActor(XGLShape *s);
+
+	void onShapeHit(const physx::PxControllerShapeHit& hit);
+	void onControllerHit(const physx::PxControllersHit& hit);
+	void onObstacleHit(const physx::PxControllerObstacleHit& hit);
+
+	void RayCast(glm::vec3, glm::vec3);
+	void ResetActive();
+
+	class PhysxRenderer : public XGLShape {
+	public:
+		PhysxRenderer(PhysXXGL *p) : container(p) {
+			box = new XGLCube();
+			ball = new XGLSphere(1.0, 18);
+			capsule = new XGLCapsule(1.0, 2.0, 18);
+		};
+		PhysXXGL *container;
+		void Draw();
+		void Init() {
+			XGLShader *shader = container->shaderMap[pathToAssets + "/shaders/diffuse"];
+			box->Load(shader, box->v, box->idx);
+			ball->Load(shader, ball->v, ball->idx);
+			capsule->Load(shader, capsule->v, capsule->idx);
+		}
+
+		XGLCube *box;
+		XGLSphere *ball;
+		XGLCapsule *capsule;
+		GLuint program;
+	};
+
+	class UserData {
+	public:
+		UserData(int i) : active(false), id(i) {};
+		bool active;
+		int id;
+	};
+
+	PhysxRenderer *renderer;
+	physx::PxActor *activeActor;
+
+	int dynamicsSerialNumber;
+	int hitId;
+
+	physx::PxDistanceJoint *mouseJoint = NULL;
+	physx::PxRigidDynamic *mouseSphere = NULL;
+	float mouseDepth;
+	physx::PxRigidDynamic *selectedActor = NULL;
+	XGLShader *shader;
+};
