e71602b6504f71e391d192e3f710634b01606cb0
evan <evan-github@madstyle.net>
2016-10-15 22:51:51 +0000

    Working on XGLMaterial.  I've punted on doing the properly until now.  This is a work in progress.

--- xclass/xgl/xgl.h
+++ xclass/xgl/xgl.h
@@ -145,7 +145,7 @@ public:
 private:
     // this is returned by GetShader().  Use of GetShader() feels funky, like my structure design blows chunks.
     XGLShader *currentShader;
-	GLuint matrixUbo, lightUbo;
+	GLuint matrixUbo, lightUbo, materialUbo;
 
     // these are used by GLUT implementations
 #ifdef USE_GLUT
--- xclass/xgl/xglmaterial.h
+++ xclass/xgl/xglmaterial.h
@@ -10,32 +10,34 @@
 #ifndef XGLMATERIAL_H
 #define XGLMATERIAL_H
 
-const XGLColor black = { 0, 0, 0, 1 };
-const XGLColor white = { 1, 1, 1, 1 };
-const XGLColor red = { 1, 0, 0, 1 };
-const XGLColor green = { 0, 1, 0, 1 };
-const XGLColor blue = { 0, 0, 1, 1 };
-const XGLColor yellow = { 1, 1, 0, 1 };
+#include "xglprimitives.h"
+
+const XGLColor black =   { 0, 0, 0, 1 };
+const XGLColor white =   { 1, 1, 1, 1 };
+const XGLColor red =     { 1, 0, 0, 1 };
+const XGLColor green =   { 0, 1, 0, 1 };
+const XGLColor blue =    { 0, 0, 1, 1 };
+const XGLColor yellow =  { 1, 1, 0, 1 };
+const XGLColor cyan =    { 0, 1, 1, 1 };
+const XGLColor magenta = { 1, 0, 1, 1 };
 
 class XGLMaterial {
 public:
-	XGLMaterial() :
-		ambientColor(white),
-		diffuseColor(white),
-		specularColor(white),
-		shininess(100.0f)
-	{};
+	XGLMaterial() {
+		m.ambientColor = white;
+		m.diffuseColor = white;
+		m.specularColor = white;
+		m.specularExponent = 0.005f;
+	};
 
 	void Bind(GLuint program) {
-		glUniform4fv(glGetUniformLocation(program, "ambientColor"), 1, glm::value_ptr(ambientColor));
+		glUniform4fv(glGetUniformLocation(program, "ambientColor"), 1, glm::value_ptr(m.ambientColor));
 		GL_CHECK("glUniform4fv() failed");
-		glUniform4fv(glGetUniformLocation(program, "diffuseColor"), 1, glm::value_ptr(diffuseColor));
+		glUniform4fv(glGetUniformLocation(program, "diffuseColor"), 1, glm::value_ptr(m.diffuseColor));
 		GL_CHECK("glUniform4fv() failed");
 	}
-	XGLColor ambientColor;
-	XGLColor diffuseColor;
-	XGLColor specularColor;
-	glm::float32 shininess;
+
+	XGLMaterialAttributes m;
 };
 
 #endif
