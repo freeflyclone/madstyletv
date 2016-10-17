ee9becb42ccaf766cf2bd81d43b2321017cf3803
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-10-17 22:14:49 +0000

    renamed XGLMaterial::a to XGLMaterial::attributes

--- examples/PhysXTest/physx-xgl.h
+++ examples/PhysXTest/physx-xgl.h
@@ -99,11 +99,11 @@ public:
 		void Draw();
 		void Init(XGLShader *shader) {
 			box->Load(shader, box->v, box->idx);
-			box->SetColor(blue);
 			box->l = shader->materialLocations;
+
 			ball->Load(shader, ball->v, ball->idx);
-			ball->SetColor(yellow);
 			ball->l = shader->materialLocations;
+
 			capsule->Load(shader, capsule->v, capsule->idx);
 			capsule->l = shader->materialLocations;
 		}
--- xclass/xgl/xglmaterial.cpp
+++ xclass/xgl/xglmaterial.cpp
@@ -1,20 +1,20 @@
 #include "xgl.h"
 
 XGLMaterial::XGLMaterial() {
-	a.ambientColor = white;
-	a.diffuseColor = white;
-	a.specularColor = white;
-	a.shininess = 120.0f;
+	attributes.ambientColor = white;
+	attributes.diffuseColor = white;
+	attributes.specularColor = white;
+	attributes.shininess = 120.0f;
 };
 
 void XGLMaterial::Bind(GLuint program) {
-	glUniform4fv(l.ambientLocation, 1, glm::value_ptr(a.ambientColor));
+	glUniform4fv(l.ambientLocation, 1, glm::value_ptr(attributes.ambientColor));
 	GL_CHECK("glUniform4fv() failed");
-	glUniform4fv(l.diffuseLocation, 1, glm::value_ptr(a.diffuseColor));
+	glUniform4fv(l.diffuseLocation, 1, glm::value_ptr(attributes.diffuseColor));
 	GL_CHECK("glUniform4fv() failed");
-	glUniform4fv(l.specularLocation, 1, glm::value_ptr(a.specularColor));
+	glUniform4fv(l.specularLocation, 1, glm::value_ptr(attributes.specularColor));
 	GL_CHECK("glUniform4fv() failed");
-	glUniform1f(l.shininessLocation, a.shininess);
+	glUniform1f(l.shininessLocation, attributes.shininess);
 	GL_CHECK("glUniform1f() failed");
 }
 
