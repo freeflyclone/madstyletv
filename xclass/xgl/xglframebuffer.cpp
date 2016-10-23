b49ad05905f8f95e8a39a7049d4aca93f4c73b07
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-10-23 18:31:17 +0000

    Added new XGLFramebuffer constructor that accepts 2 GLuint textureIDs, and uses these instead of generating it's own.  This is probably the key change that has FBO -> texture source finally working. (somewhat) There is still some sort of issue that is render order dependent, it appears to be fighting over which texture to use for rendering, it is visually flashing.

--- xclass/xgl/xglframebuffer.cpp
+++ xclass/xgl/xglframebuffer.cpp
@@ -1,12 +1,65 @@
 #include "xgl.h"
 
+XGLFramebuffer::XGLFramebuffer(int w, int h, GLuint tex0, GLuint tex1) :
+	XGLObject("XGLFramebuffer"),
+	width(w),
+	height(h)
+{
+	textures[0] = tex0;
+	textures[1] = tex1;
+
+	const int nSamples = 8;
+
+	attachments[0] = GL_COLOR_ATTACHMENT0;
+	attachments[1] = GL_COLOR_ATTACHMENT1;
+
+	xprintf("XGLFramebuffer::XGLFramebuffer()\n");
+
+	glGenFramebuffers(1, &fbo);
+	GL_CHECK("glGenFramebuffers() failed");
+
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	GL_CHECK("glBindFramebuffer() failed");
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textures[0], 0);
+	GL_CHECK("glFramebufferTexture() failedn");
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, textures[1], 0);
+	GL_CHECK("glFramebufferTexture() failedn");
+
+	glDrawBuffers(2, attachments);
+	GL_CHECK("glDrawBuffers() failed");
+
+	// The depth buffer
+	glGenRenderbuffers(1, &depth);
+	GL_CHECK("glGenRenderbuffers() failed");
+
+	glBindRenderbuffer(GL_RENDERBUFFER, depth);
+	GL_CHECK("glBindRenderbuffer() failed");
+
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+	GL_CHECK("glRenderbufferStorage() failed");
+
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth);
+	GL_CHECK("glFramebufferRenderbuffer() failed");
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		xprintf("glCheckFramebufferStatus() != GL_FRAMEBUFFER_COMPLETE\n");
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	GL_CHECK("glBindFrameBuffer(0) failed");
+}
+
 XGLFramebuffer::XGLFramebuffer(int w, int h) :
 	XGLObject("XGLFramebuffer"),
 	width(w),
 	height(h)
 {
 	const int nSamples = 8;
-	const GLuint attachments[] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+
+	attachments[0] = GL_COLOR_ATTACHMENT0;
+	attachments[1] = GL_COLOR_ATTACHMENT1;
+
 	xprintf("XGLFramebuffer::XGLFramebuffer()\n");
 
 	glGenFramebuffers(1, &fbo);
