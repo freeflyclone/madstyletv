3dd8ead5438032aabe2e7f0eb135ea7bb780252b
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-10-25 06:08:18 +0000

    XGLSharePBO now works, but it's not actually PBO.  Just using glReadPixels() for now to replace what XGLSharedFBO did.  This was really easy, but it lacks anti-aliasing and scaling to arbitrary sized windows.  I'll need another method for that.

--- xclass/xgl/xglpixelbuffer.cpp
+++ xclass/xgl/xglpixelbuffer.cpp
@@ -1,20 +1,16 @@
 #include "xgl.h"
 
-XGLPixelbuffer::XGLPixelbuffer(int w, int h) {
-
-}
+XGLPixelbuffer::XGLPixelbuffer(int w, int h) : width(w), height(h){}
 
 XGLPixelbuffer::~XGLPixelbuffer() {}
 
-void XGLPixelbuffer::Render() {
-	xprintf("XGLPixelbuffer::Render()\n");
+void XGLPixelbuffer::Render(unsigned char *b) {
+	glReadPixels(0, 0, width, height, GL_BGR, GL_UNSIGNED_BYTE, b);
 }
 
 void XGLSharedPBO::Render() {
 	width = pHeader->width;
 	height = pHeader->height;
 
-	xprintf("XGLSharedPBO::Render()\n");
-
-	XGLPixelbuffer::Render();
+	XGLPixelbuffer::Render(mappedBuffer);
 }
