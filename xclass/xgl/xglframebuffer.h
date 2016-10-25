e106031d4f105f76b76de080f1f7b783c625e85e
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-10-25 22:39:20 +0000

    Modifications for restoring original FBO functionality.

--- xclass/xgl/xglframebuffer.h
+++ xclass/xgl/xglframebuffer.h
@@ -8,12 +8,13 @@
 #ifndef XGLFRAMEBUFFER_H
 #define XGLFRAMEBUFFER_H
 #include "xglobject.h"
+#include "xshmem.h"
 
 typedef std::function<void()> XGLFBORender;
 
 class XGLFramebuffer : public XGLObject {
 public:
-	XGLFramebuffer(int w, int h, GLuint *texs, int ntex);
+	XGLFramebuffer(int w, int h, GLuint *texs, int ntex, bool withDepth = true);
 	virtual ~XGLFramebuffer();
 
 	virtual void Render(XGLFBORender);
@@ -27,6 +28,21 @@ public:
 	// dimensions (used for glViewport())
 	int width, height;
 	int numTextures;
+
+	// an FBO being used as a destination for glBlitFramebuffer()
+	// (for example, from system FBO to shared memory) doesn't
+	// require depth buffer, only color.
+	bool hasDepth;
+};
+
+class XGLSharedFBO : public XSharedMem {
+public:
+	XGLSharedFBO();
+
+	virtual void Render(int w, int h);
+
+	XGLFramebuffer *fbo;
+	XGLFramebuffer *intFbo;
 };
 
 #endif
