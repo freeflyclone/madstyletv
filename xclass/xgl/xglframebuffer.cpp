13cbef8b93307068c60718533f841a968736d011
evan <evan-github@madstyle.net>
2016-10-31 18:17:05 +0000

    Scaling for shard memory viewport size is working, but the image is inverted. Again. :(

--- xclass/xgl/xglframebuffer.cpp
+++ xclass/xgl/xglframebuffer.cpp
@@ -145,7 +145,6 @@ XGLSharedFBO::XGLSharedFBO(XGL *context) : XSharedMem(DEFAULT_FILE_NAME), pXGL(c
 void XGLSharedFBO::MakeFlipQuad() {
 	pXGL->CreateShape("shaders/imageflip", [&]() { flipQuad = new XGLTexQuad(); return flipQuad; });
 	flipQuad->AddTexture(scaleFbo->textures[0]);
-	flipQuad->model = glm::translate(glm::mat4(), glm::vec3(0, -0.6667, 0));
 }
 
 void XGLSharedFBO::RenderFlipQuad() {
