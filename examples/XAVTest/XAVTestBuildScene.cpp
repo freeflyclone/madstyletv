7019ee8b05d88f7f0a7dc504d402debf28f02eef
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-11-02 23:10:49 +0000

    And then it was so.  YUV -> RGB in a fragment shader.  With 4:2:0 automagically handled by the texturing units of the hardware.  That's pretty awesome!

--- examples/XAVTest/XAVTestBuildScene.cpp
+++ examples/XAVTest/XAVTestBuildScene.cpp
@@ -11,8 +11,14 @@
 
 #include <iostream>
 
+#define VIDEO_WIDTH 1920
+#define VIDEO_HEIGHT 1080
+#define VIDEO_CHANNELS 4
+
 typedef struct {
-	unsigned char y[1920 * 1080];
+	unsigned char y[VIDEO_WIDTH * VIDEO_HEIGHT];
+	unsigned char u[VIDEO_WIDTH * VIDEO_HEIGHT];
+	unsigned char v[VIDEO_WIDTH * VIDEO_HEIGHT];
 	int width, height;
 } ImageBuff;
 
@@ -30,6 +36,8 @@ public:
 				break;
 			}
 			memcpy(&ib.y, image.buffers[0], sizeof(ib.y));
+			memcpy(&ib.u, image.buffers[1], sizeof(ib.u));
+			memcpy(&ib.v, image.buffers[2], sizeof(ib.v));
 			ib.width = stream->width;
 			ib.height = stream->height;
 		}
@@ -174,10 +182,6 @@ public:
 
 AVPlayer *pavp;
 
-#define VIDEO_WIDTH 1920
-#define VIDEO_HEIGHT 1080
-#define VIDEO_CHANNELS 4
-
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
 
@@ -194,8 +198,8 @@ void ExampleXGL::BuildScene() {
 	});
 
 	AddShape("shaders/yuv", [&](){ shape = new XGLTexQuad(VIDEO_WIDTH,VIDEO_HEIGHT,VIDEO_CHANNELS); return shape; });
-	shape->AddTexture(VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_CHANNELS);
-	shape->AddTexture(VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_CHANNELS);
+	shape->AddTexture(VIDEO_WIDTH/2, VIDEO_HEIGHT/2, VIDEO_CHANNELS);
+	shape->AddTexture(VIDEO_WIDTH/2, VIDEO_HEIGHT/2, VIDEO_CHANNELS);
 
 	glm::mat4 scale = glm::scale(glm::mat4(), glm::vec3(10.0f, 5.625f, 1.0f));
 	glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0, 4.0f, 5.625f));
@@ -204,9 +208,24 @@ void ExampleXGL::BuildScene() {
 
 	XGLShape::AnimaFunk transform = [&](XGLShape *s, float clock) {
 		if (pavp != NULL && pavp->IsRunning()) {
-			unsigned char *image = ib.y;
 
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, ib.width, ib.height, 0, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)image);
+			glProgramUniform1i(s->shader->programId, glGetUniformLocation(s->shader->programId, "texUnit0"), 0);
+			glProgramUniform1i(s->shader->programId, glGetUniformLocation(s->shader->programId, "texUnit1"), 1);
+			glProgramUniform1i(s->shader->programId, glGetUniformLocation(s->shader->programId, "texUnit2"), 2);
+
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, s->texIds[0]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, ib.width, ib.height, 0, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)ib.y);
+			GL_CHECK("glGetTexImage() didn't work");
+
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, s->texIds[1]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, ib.width / 2, ib.height / 2, 0, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)ib.u);
+			GL_CHECK("glGetTexImage() didn't work");
+
+			glActiveTexture(GL_TEXTURE2);
+			glBindTexture(GL_TEXTURE_2D, s->texIds[2]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, ib.width / 2, ib.height / 2, 0, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)ib.v);
 			GL_CHECK("glGetTexImage() didn't work");
 		}
 	};
