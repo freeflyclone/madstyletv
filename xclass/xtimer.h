c312e6a0d5114ed30484d0ba8f700da325b8f074
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-11-03 21:41:14 +0000

    Added XTimer class to xclass library.  Uses QueryPerformanceCounter() on Windows, std::chrono::high_resolution_timer elsewhere (unknown at this point how well that works)
    Modified XAVText example to make use of it for video streams.  Unknown how it plays with videos that have audio.  Time to test that.

---
+++ xclass/xtimer.h
@@ -0,0 +1,57 @@
+#ifndef XTIMER_H
+#define XTIMER_H
+
+#include <chrono>
+#include <ctime>
+#include <thread>
+
+#include "xutils.h"
+
+#ifdef _WIN32
+class XTimer {
+public:
+	XTimer() {
+		QueryPerformanceFrequency(&frequency);
+		QueryPerformanceCounter(&epoch);
+	}
+	double Since() {
+		QueryPerformanceCounter(&since);
+		double diff = (double)(since.QuadPart - epoch.QuadPart) / (double)frequency.QuadPart;
+		return diff;
+	}
+	double SinceLast() {
+		QueryPerformanceCounter(&since);
+		double diff = (double)(since.QuadPart - last.QuadPart) / (double)frequency.QuadPart;
+		last = since;
+		return diff;
+	}
+
+	LARGE_INTEGER epoch,since,last;
+	LARGE_INTEGER frequency;
+};
+#else
+class XTimer {
+public:
+	XTimer() {
+		epoch = std::chrono::high_resolution_clock::now();
+	};
+
+	double Since() {
+		since = std::chrono::high_resolution_clock::now();
+		diff = since - epoch;
+		return diff.count();
+	}
+	double SinceLast() {
+		since = std::chrono::high_resolution_clock::now();
+		diff = since - last;
+		last = since;
+		return diff.count();
+	}
+
+	std::chrono::time_point<std::chrono::high_resolution_clock> epoch,since,last;
+	std::chrono::duration<double> diff;
+
+};
+#endif
+
+#endif
