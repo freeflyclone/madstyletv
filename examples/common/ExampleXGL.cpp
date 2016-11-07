2136a52466a301c1fd0378799bfd393e9dbb2b0e
evan <evan-github@madstyle.net>
2016-11-07 19:11:38 +0000

    Rearranging the XGLGuiCanvas a bit.

--- examples/common/ExampleXGL.cpp
+++ examples/common/ExampleXGL.cpp
@@ -27,6 +27,14 @@ ExampleXGL::ExampleXGL() : wc(&shaderMatrix) {
 	// on a per example basis.
 	BuildScene();
 
+	// shapes are sorted in an std::map by name, and I want the GUI canvas to be
+	// rendered last so the transparency will actually work.
+	// TODO: A better way is needed than depending on sort order of the shader name.
+	AddShape("shaders/zz-gui", [&]() { shape = new XGLGuiCanvas(16, 9); return shape; });
+
+	// set the canvas color alpha to less than 1.0 for some transparency
+	shape->SetColor({ 1.0, 1.0, 1.0, 0.5 });
+
 	XInputKeyFunc PresentGuiCanvas = [&](int key, int flags) {
 		const bool isDown = (flags & 0x8000) == 0;
 		const bool isRepeat = (flags & 0x4000) != 0;
@@ -37,10 +45,6 @@ ExampleXGL::ExampleXGL() : wc(&shaderMatrix) {
 
 	AddKeyFunc('`', PresentGuiCanvas);
 	AddKeyFunc('~', PresentGuiCanvas);
-
-	AddShape("shaders/zz-gui", [&]() { shape = new XGLGuiCanvas(16,9); return shape; });
-	shape->SetColor({ 1.0, 0.0, 1.0, 0.5 });
-	//shape->AddTexture(pathToAssets + "/assets/AndroidDemo.png");
 }
 
 void ExampleXGL::CameraTracker(XGLCamera *c){
