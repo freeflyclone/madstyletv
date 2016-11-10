e14c6bda026e3c4ee406aebef1d7a9c396a1aaea
evan <evan-github@madstyle.net>
2016-11-10 20:09:12 +0000

    First step of getting some actual text rendering going. As the font is being parsed and the atlas bitmaps are being built, build an std::map to allow for retrieving all of the necessary texture mapping data.
    Goal is to be able to create a line of text as a triangle strip of N quads, where N is the number of characters in the line of text.

--- xclass/xgl/xglfont.cpp
+++ xclass/xgl/xglfont.cpp
@@ -50,112 +50,133 @@ XGLFont::XGLFont() : atlasWidth(0), atlasHeight(0), atlasPageCount(0), bitmapPag
     // scale the rendering to 1 pixel per 'point' resolution
     FT_Set_Pixel_Sizes(face, 0, 64);
 
-    FT_GlyphSlot g = face->glyph;
+	FT_GlyphSlot g = face->glyph;
 
-    //if (false)
-    {
-        FT_ULong charcode;
-        FT_UInt gindex;
+	//if (false)
+	{
+		FT_ULong charcode;
+		FT_UInt gindex;
+		XGLGlyph xGlyph;
 
-        // build an XGLCharMap of the entire set of glyphs for this font.
-        // (this could be huge for Chinese fonts)
-        for (charcode = FT_Get_First_Char(face, &gindex); gindex; charcode = FT_Get_Next_Char(face, charcode, &gindex))
-            charMap.emplace(charcode, gindex);
+		// build an XGLCharMap of the entire set of glyphs for this font.
+		// (this could be huge for Chinese fonts)
+		for (charcode = FT_Get_First_Char(face, &gindex); gindex; charcode = FT_Get_Next_Char(face, charcode, &gindex))
+			charMap.emplace(charcode, gindex);
 
-        const int glyphsPerRow = 32;
-        const int rowsPerBitmap = 32;
+		const int glyphsPerRow = 32;
+		const int rowsPerBitmap = 32;
 
-        // face->num_glyphs isn't necessarily accurate
-        const int numGlyphs = (const int)(charMap.size());
-        const int glyphsPerBitmap = glyphsPerRow * rowsPerBitmap;
-        const int numBitmaps = numGlyphs / glyphsPerBitmap + 1;
+		// face->num_glyphs isn't necessarily accurate
+		const int numGlyphs = (const int)(charMap.size());
+		const int glyphsPerBitmap = glyphsPerRow * rowsPerBitmap;
+		const int numBitmaps = numGlyphs / glyphsPerBitmap + 1;
 
-        int maxRowHeight = 0;
-        int maxRowWidth = 0;
+		int maxRowHeight = 0;
+		int maxRowWidth = 0;
 
 		xprintf("There are %d glyphs.\n", numGlyphs);
 
-        XGLFont::CharMap::iterator it = charMap.begin();
+		XGLFont::CharMap::iterator it = charMap.begin();
 
-        // iterate throw the entire charmap...
-        // one "glyph row" at a time...
-        // measure the total width of the row...
-        // and set maxRowWidth and maxRowHeight...
-        // so we can determine the size of an atlas rectangle.
-        // (no, there's no optimal packing going on here)
-        for (unsigned int i = 0; i < charMap.size(); i++, it++) {
-            int rowHeight = 0;
-            int rowWidth = 0;
-            for (int j = 0; j < glyphsPerRow && it != charMap.end(); j++, it++) {
-                FT_Load_Glyph(face, it->second, FT_LOAD_DEFAULT);
-                if (g->metrics.height / 64 > rowHeight)
-                    rowHeight = g->metrics.height / 64;
-                rowWidth += g->metrics.width / 64;
-            }
-            if (it == charMap.end())
-                break;
-            if (rowWidth > maxRowWidth)
-                maxRowWidth = rowWidth;
-            if (rowHeight > maxRowHeight)
-                maxRowHeight = rowHeight;
-        }
+		// iterate throw the entire charmap...
+		// one "glyph row" at a time...
+		// measure the total width of the row...
+		// and set maxRowWidth and maxRowHeight...
+		// so we can determine the size of an atlas rectangle.
+		// (no, there's no optimal packing going on here)
+		for (unsigned int i = 0; i < charMap.size(); i++, it++) {
+			int rowHeight = 0;
+			int rowWidth = 0;
+			for (int j = 0; j < glyphsPerRow && it != charMap.end(); j++, it++) {
+				FT_Load_Glyph(face, it->second, FT_LOAD_DEFAULT);
+				if (g->metrics.height / 64 > rowHeight)
+					rowHeight = g->metrics.height / 64;
+				rowWidth += g->metrics.width / 64;
+			}
+			if (it == charMap.end())
+				break;
+			if (rowWidth > maxRowWidth)
+				maxRowWidth = rowWidth;
+			if (rowHeight > maxRowHeight)
+				maxRowHeight = rowHeight;
+		}
 
-        // now we know how tall a texture atlas needs to be
-        const int bitmapHeight = maxRowHeight * rowsPerBitmap;
+		// now we know how tall a texture atlas needs to be
+		const int bitmapHeight = maxRowHeight * rowsPerBitmap;
 
-        // we already figured how many texture atlas bitmaps we'll need for the entire
-        // glyph set, so allocate a set of pointers to those bitmap pages
-        if ((bitmapPages = (GLubyte **)malloc(numBitmaps*sizeof(GLubyte *))) == NULL)
-            throwXGLException("malloc() failed allocating list of bitmap pages");
+		// we already figured how many texture atlas bitmaps we'll need for the entire
+		// glyph set, so allocate a set of pointers to those bitmap pages
+		if ((bitmapPages = (GLubyte **)malloc(numBitmaps*sizeof(GLubyte *))) == NULL)
+			throwXGLException("malloc() failed allocating list of bitmap pages");
 
-        memset(bitmapPages, 0, sizeof(GLubyte *)*numBitmaps);
+		memset(bitmapPages, 0, sizeof(GLubyte *)*numBitmaps);
 
-        // expose the dimensions we've calculated
-        atlasWidth = maxRowWidth;
-        atlasHeight = bitmapHeight;
-        atlasPageCount = numBitmaps;
+		// expose the dimensions we've calculated
+		atlasWidth = maxRowWidth;
+		atlasHeight = bitmapHeight;
+		atlasPageCount = numBitmaps;
 
-        //rewind the iterator
-        it = charMap.begin();
+		//rewind the iterator
+		it = charMap.begin();
 
-        // fill up all alocated pages with glyph images.
-        for (int l = 0; l < numBitmaps; l++) {
-            if ((bitmapPages[l] = (GLubyte *)malloc(maxRowWidth*bitmapHeight)) == NULL)
-                throwXGLException("malloc() failed allocating a texture atlas page");
+		// fill up all alocated pages with glyph images.
+		for (int l = 0; l < numBitmaps; l++) {
+			if ((bitmapPages[l] = (GLubyte *)malloc(maxRowWidth*bitmapHeight)) == NULL)
+				throwXGLException("malloc() failed allocating a texture atlas page");
 
-            memset(bitmapPages[l], 0, maxRowWidth*bitmapHeight);
+			memset(bitmapPages[l], 0, maxRowWidth*bitmapHeight);
 
-            // set "dest" once per page, it gets incremented vertically by the
-            // size in bytes of a glyph row, every row.
-            GLubyte *dest = bitmapPages[l];
+			// set "dest" once per page, it gets incremented vertically by the
+			// size in bytes of a glyph row, every row.
+			GLubyte *dest = bitmapPages[l];
 
-            // fill up all rows...
-            for (int k = 0; k < rowsPerBitmap; k++){
-                int xOffset = 0;
-                // fill up this row with its set of glyphs
-                for (int j = 0; j < glyphsPerRow && it!=charMap.end(); j++, it++){
-                    // get the glyph
-                    FT_Load_Glyph(face, it->second, FT_LOAD_RENDER);
+			// fill up all rows...
+			for (int k = 0; k < rowsPerBitmap; k++){
+				int xOffset = 0;
+				// fill up this row with its set of glyphs
+				for (int j = 0; j < glyphsPerRow && it != charMap.end(); j++, it++){
+					// get the glyph
+					FT_Load_Glyph(face, it->second, FT_LOAD_RENDER);
 
-                    // and blit it to the this texture atlas page at the current 
-                    // "xOffset" and current glyph row...
-                    // (this is classic 2D blit C code.)
-                    GLubyte *src = (GLubyte *)g->bitmap.buffer;
-                    for (unsigned int i = 0; i < g->bitmap.rows; i++) {
-                        memcpy((dest + (i*atlasWidth) + (xOffset)), src, g->bitmap.width);
-                        src += g->bitmap.width;
-                    }
+					// and blit it to the this texture atlas page at the current 
+					// "xOffset" and current glyph row...
+					// (this is classic 2D blit C code.)
+					GLubyte *src = (GLubyte *)g->bitmap.buffer;
+					for (unsigned int i = 0; i < g->bitmap.rows; i++) {
+						memcpy((dest + (i*atlasWidth) + (xOffset)), src, g->bitmap.width);
+						src += g->bitmap.width;
+					}
 
-                    xOffset += g->advance.x / 64;
-                }
-                dest += maxRowWidth * maxRowHeight;
-                if (it == charMap.end())
-                    break;
-            }
-            if (it == charMap.end())
-                break;
-        }
-    }
+					// Start: where XGLGlyph should be built and appended to glyphMap
+					xGlyph.index = it->second;
+					xGlyph.page = l;
+					xGlyph.row = k;
+					xGlyph.xOff = xOffset;
+					xGlyph.yOff = k * maxRowHeight;
+					xGlyph.width = g->bitmap.width;
+					xGlyph.height = g->bitmap.rows;
+					glyphMap.emplace(it->first, xGlyph);
+					// End: adding XGLGlyph to glyphMap
+
+					xOffset += g->advance.x / 64;
+				}
+				dest += maxRowWidth * maxRowHeight;
+				if (it == charMap.end())
+					break;
+			}
+			if (it == charMap.end())
+				break;
+		}
+		// at this point there should be a 1:1 between charMap and glyphMap.
+		XGLFont::CharMap::iterator cit = charMap.begin();
+		XGLFont::GlyphMap::iterator git = glyphMap.begin();	
+		for (; git != glyphMap.end(); cit++, git++){
+			XGLGlyph xg = git->second;
+			FT_UInt index = cit->second;
+			if (xg.index != index)
+				xprintf("MisMatch: %d vs %d\n", index, xg.index);
+		}		
+	}
 }
 
 XGLFont::~XGLFont(){
