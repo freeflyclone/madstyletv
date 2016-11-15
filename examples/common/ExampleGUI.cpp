1431811505ce4219a89d00bff4104ffc22743192
evan <evan-github@madstyle.net>
2016-11-15 02:32:41 +0000

    3rd GUI window is starting to become a slider for seeking video.  Time to Netflix tho.

--- examples/common/ExampleGUI.cpp
+++ examples/common/ExampleGUI.cpp
@@ -59,58 +59,12 @@ void ExampleXGL::BuildGUI() {
 	});
 
 	CreateShape(&guiShapes, "shaders/gui-tex", [&]() { child1 = new XGLGuiCanvas(1920, 1080); return child1; });
-	translate = glm::translate(glm::mat4(), glm::vec3(0.5, -0.5f, 0));
-	model = glm::scale(translate, glm::vec3(0.48, 0.48, 1.0));
+	translate = glm::translate(glm::mat4(), glm::vec3(-0., -0.8f, 0));
+	model = glm::scale(translate, glm::vec3(0.99, 0.05, 1.0));
 	child1->model = model;
 	child1->attributes.diffuseColor = { 1.0, 1.0, 1.0, 0.7 };
 	GetGuiRoot()->AddChild(child1);
 	child1->SetMouseFunc([&](XGLShape *s, float x, float y, int flags) {
 		xprintf("In MouseFunc() for %s : %0.4f, %0.4f\n", s->name.c_str(), x, y);
 	});
-
-	child1->RenderText(L"This text *might* show up.\n");
-	child1->RenderText(L"It might not.\n\n");
-	child1->RenderText(L"Sed ut perspiciatis, unde omnis iste natus error sit voluptatem\n");
-	child1->RenderText(L"accusantium doloremque laudantium, totam rem aperiam eaque\n");
-	child1->RenderText(L"ipsa, quae ab illo inventore veritatis et quasi architecto\n");
-	child1->RenderText(L"beatae vitae dicta sunt, explicabo.\n\n");
-	child1->RenderText(L"But I must explain to you how all this mistaken idea of\n");
-	child1->RenderText(L"denouncing of a pleasure and praising pain was born and\n");
-	child1->RenderText(L"I will give you a complete account of the system, and\n");
-	child1->RenderText(L"expound the actual teachings of the great explorer of\n");
-	child1->RenderText(L"the truth, the master - builder of human happiness.\n");
-
-	CreateShape(&guiShapes, "shaders/gui", [&]() { child3 = new XGLTexQuad(); return child3; });
-	translate = glm::translate(glm::mat4(), glm::vec3(-0.5, 0.5, 0));
-	model = glm::scale(translate, glm::vec3(0.5, 0.5, 1.0));
-	child3->model = model;
-	child3->attributes.diffuseColor = { 1.0, 1.0, 1.0, 0.1 };
-	child3->AddTexture(pathToAssets + "/assets/yellow.png");
-	child1->AddChild(child3);
-
-	/*
-	CreateShape(&guiShapes, "shaders/gui", [&]() { child3 = new XGLTexQuad(); return child3; });
-	translate = glm::translate(glm::mat4(), glm::vec3(0.5, 0.5, 0));
-	model = glm::scale(translate, glm::vec3(0.5, 0.5, 1.0));
-	child3->model = model;
-	child3->attributes.diffuseColor = { 1.0, 1.0, 1.0, 0.1 };
-	child3->AddTexture(pathToAssets + "/assets/green.png");
-	child1->AddChild(child3);
-
-	CreateShape(&guiShapes, "shaders/gui", [&]() { child3 = new XGLTexQuad(); return child3; });
-	translate = glm::translate(glm::mat4(), glm::vec3(-0.5, -0.5, 0));
-	model = glm::scale(translate, glm::vec3(0.5, 0.5, 1.0));
-	child3->model = model;
-	child3->attributes.diffuseColor = { 1.0, 1.0, 1.0, 0.1 };
-	child3->AddTexture(pathToAssets + "/assets/red.png");
-	child1->AddChild(child3);
-
-	CreateShape(&guiShapes, "shaders/gui", [&]() { child3 = new XGLTexQuad(); return child3; });
-	translate = glm::translate(glm::mat4(), glm::vec3(0.5, -0.5, 0));
-	model = glm::scale(translate, glm::vec3(0.5, 0.5, 1.0));
-	child3->model = model;
-	child3->attributes.diffuseColor = { 1.0, 1.0, 1.0, 0.1 };
-	child3->AddTexture(pathToAssets + "/assets/blue.png");
-	child1->AddChild(child3);
-	*/
 }
