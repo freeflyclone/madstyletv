5fca7726f6a37b1e8870a53e2f077ca20623ee16
evan <evan-github@madstyle.net>
2016-11-19 02:39:53 +0000

    Fixed a bug in XAL::XAL() power-of-two test. Re-enabled back face culling.

--- xclass/xgl/xgl.cpp
+++ xclass/xgl/xgl.cpp
@@ -123,8 +123,8 @@ XGL::XGL() : XGLObject("XGL"), clock(0.0f), pb(NULL), fb(NULL), renderGui(false)
 	glEnable(GL_DEPTH_TEST);
 	//glEnable(GL_BLEND);
 	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	//glEnable(GL_CULL_FACE);
-	//glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 
 	// for copying to shared memory buffer
 	// enabling this takes up GPU time,
