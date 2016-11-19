1a919aa7571900d33adab91d5f813387bf864a93
evan <evan-github@madstyle.net>
2016-11-19 22:45:39 +0000

    Got both Debug and Release configurations of glyphy for both 32 and 64 bit platforms functioning correctly in the building of GlyphyTest.

--- examples/GlyphyTest/GlyphyTestBuildScene.cpp
+++ examples/GlyphyTest/GlyphyTestBuildScene.cpp
@@ -7,8 +7,21 @@
 **************************************************************/
 #include "ExampleXGL.h"
 
+#include "config.h"
+#include "demo-buffer.h"
+#include "demo-font.h"
+#include "demo-view.h"
+
+static demo_glstate_t *st;
+static demo_view_t *vu;
+static demo_buffer_t *buffer;
+
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
+
+	st = demo_glstate_create();
+	//vu = demo_view_create(st);
+	//demo_view_print_help(vu);
 
 	AddShape("shaders/simple", [&](){ shape = new XGLTriangle(); return shape; });
 }
