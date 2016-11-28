844a6a5b9527d9dfa8e75d31782d71a45c15c19b
evan <evan-github@madstyle.net>
2016-11-28 17:21:31 +0000

    Changed the compute shader demo to have the output image be RGBA32F

--- examples/ComputeShaderTest/ComputeShaderTestBuildScene.cpp
+++ examples/ComputeShaderTest/ComputeShaderTestBuildScene.cpp
@@ -30,11 +30,11 @@ void ExampleXGL::BuildScene() {
 	glBindTexture(GL_TEXTURE_2D, texHandle);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, 512, 512, 0, GL_RED, GL_FLOAT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, 512, 512, 0, GL_RGBA, GL_FLOAT, NULL);
 
 	// Because we're also using this tex as an image (in order to write to it),
 	// we bind it to an image unit as well
-	glBindImageTexture(0, texHandle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_R32F);
+	glBindImageTexture(0, texHandle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 	GL_CHECK("Gen texture");
 
 	shape->AddTexture(texHandle);
