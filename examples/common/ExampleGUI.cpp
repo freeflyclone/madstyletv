202b3341a4bacb25f81c7b2bd3bf75ff911be497
evan <evan-github@madstyle.net>
2016-12-01 02:31:29 +0000

    Added some comments explaining the contents of ExampleGUI.cpp

--- examples/common/ExampleGUI.cpp
+++ examples/common/ExampleGUI.cpp
@@ -1,9 +1,34 @@
 #include "ExampleXGL.h"
 
+/*
+** GuiManager class
+**
+**	Adds a ReshapeCallback layer to XGLGuiCanvas items, and serves as the GuiRoot() shape.
+**	The primary function of GuiManager is to manage the layout of child XGLGuiCanvas
+**	items by allowing XGLGuiCanvas items that care about window sizing events to
+**	get notification via callback functions.  It is envisioned that this will apply
+**	mostly XGLGuiCanvas items that are intended to hug the right and/or bottom edges
+**  of the main window.
+**
+**	Additionally, an XInput::XInputKeyFunc is added to allow dynamic toggling of
+**  the GUI layer.  The GUI layer is maintained by XGL as a separate list of
+**	XGLShape items.  This allows leveraging the existing hierarchy mechanism
+**	already established for XGLShapes.
+**
+**  The window resizing functionality is provided by XGLProjector, since it needs
+**	to know the window dimensions for the glViewport() call and was the first
+**  class to handle OS-specific window sizing events.  GuiManager specializes
+**	that mechanism to provide context for XGLGuiCanvas items that care about window
+**	size events, as not all XGLGuiCanvas items need to care.
+**
+**  The implementation allows for XGLGuiCanvas items to specify lambda functions
+**  as needed for the ReshapeCallback functions, which allows a fine granularity
+**	in functional specificity without excessive sub-classing.
+*/
 class GuiManager : public XGLTransformer {
 public:
-	typedef std::function<void(XGLGuiCanvas *, int, int)> ReshapeFunc;
-	typedef std::pair<XGLGuiCanvas *, ReshapeFunc> ReshapePair;
+	typedef std::function<void(XGLGuiCanvas *, int, int)> ReshapeCallback;
+	typedef std::pair<XGLGuiCanvas *, ReshapeCallback> ReshapePair;
 	typedef std::vector<ReshapePair> ReshapeCallbackList;
 
 	GuiManager(XGL *xgl, bool addTexture = false) : pxgl(xgl), padding(20) {
@@ -25,7 +50,7 @@ public:
 		xgl->projector.AddReshapeCallback(std::bind(&GuiManager::Reshape, this, _1, _2));
 	}
 
-	void AddReshapeCallback(XGLGuiCanvas *s, ReshapeFunc fn) {
+	void AddReshapeCallback(XGLGuiCanvas *s, ReshapeCallback fn) {
 		reshapeCallbacks.push_back(ReshapePair(s, fn));
 	}
 
@@ -44,27 +69,31 @@ void ExampleXGL::BuildGUI() {
 	GuiManager *gm;
 	XGLGuiCanvas *g, *g2;
 
+	// Instantiate a GuiManager shape, which serves as GuiRoot() shape.
+	// XGL::GuiResolve() requires a "place holder" as the root shape to
+	// allow for recursively passing mouse events to the XGLGuiCanvas hierarchy.
 	AddGuiShape("shaders/ortho", [&]() { gm = new GuiManager(this); return gm; });
 
+	// The first XGLGuiCanvas in the stack, henceforth the "background canvas", is the "bottom-most" 
+	// in Z stack order and traps mouse events so that they don't leak into the 
+	// 3D world mouse event handling while the GUI layer is being presented.
+	// This shape specifies a ReshapeCallback to allow it to always exactly cover the window
 	AddGuiShape("shaders/ortho", [&]() { g = new XGLGuiCanvas(this, 1, 1, false); return g; });
-	g->attributes.diffuseColor = { 1.0, 1.0, 1.0, 0.1 };
+	g->attributes.diffuseColor = { 1.0, 1.0, 1.0, 0.05 };
 	g->SetMouseFunc([&](XGLShape *s, float x, float y, int flags){
-		xprintf("In %s(%0.0f,%0.0f)\n", s->name.c_str(), x, y);
-
 		if (flags & 1)
 			mouseCaptured = (XGLGuiCanvas *)s;
 		else
 			mouseCaptured = NULL;
-
 		return true;
 	});
 	gm->AddReshapeCallback(g, [](XGLGuiCanvas *gc, int w, int h) {
-		xprintf("%d,%d in %s\n", w, h, gc->name.c_str());
 		gc->width = w;
 		gc->height = h;
 		gc->Reshape(0, 0, w, h);
 	});
 
+	// All subsequent XGLGuiCanvas items should be children of the background canvas (I think)
 	CreateShape(&guiShapes, "shaders/ortho", [&]() { g2 = new XGLGuiCanvas(this, 360, 640); return g2; });
 	g2->model = glm::translate(glm::mat4(), glm::vec3(800, 20, 0));
 	g2->attributes.diffuseColor = { 1.0, 0.0, 1.0, 0.1 };
@@ -77,11 +106,14 @@ void ExampleXGL::BuildGUI() {
 		return true;
 	});
 	gm->AddReshapeCallback(g2, [](XGLGuiCanvas *gc, int w, int h) {
-		xprintf("%d,%d in %s\n", w, h, gc->name.c_str());
 		gc->model = glm::translate(glm::mat4(), glm::vec3(w - gc->width - 20, 20, 1.0));
 	});
 	g->AddChild(g2);
 
+	// The final window in the GUI stack is a wrapper for AntTweakBar, for development
+	// purposes.  Not sure if I'm gonna keep this, but it seemed like a good idea
+	// when I integrated it.  Unfortunately, it doesn't look professional enough
+	// for end-product use, IMHO.
 	AddGuiShape("shaders/tex", [&]() { return new XGLAntTweakBar(this); });
 
 	return;
