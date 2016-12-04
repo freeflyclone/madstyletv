fd3f3b66c0c465ba019519d9b4c0c74e6a4d2242
evan <evan-github@madstyle.net>
2016-12-04 21:20:12 +0000

    Copied from the linux folder.  That one compiled correctly.

--- examples/PhysXTest/physx-main.cpp
+++ examples/PhysXTest/physx-main.cpp
@@ -1,8 +1,7 @@
 // physx-main.cpp : Defines the entry point for the GLFW-based PhysX application.
 // The console window is immediately closed.
-#ifdef _WIN32
-#include <Windows.h>
-#endif
+#include <PxPhysicsAPI.h>
+
 #include <stdio.h>
 
 #include <glew.h>
@@ -12,30 +11,6 @@
 #include <physx-xgl.h>
 
 static PhysXXGL *pxgl = NULL;
-
-void SetGlobalWorkingDirectoryName()
-{
-	DWORD sizeNeeded = GetCurrentDirectory(0, NULL);
-	DWORD size;
-	TCHAR *buff = new TCHAR[sizeNeeded];
-
-	if ((size = GetCurrentDirectory(sizeNeeded, buff)) != sizeNeeded - 1)
-		throwXGLException("GetCurrentDirectory() unexpectedly failed. " + std::to_string(size) + " vs " + std::to_string(sizeNeeded));
-
-#ifdef UNICODE
-	std::wstring wstr(buff);
-	currentWorkingDir = std::string(wstr.begin(), wstr.end());
-#else
-	currentWorkingDir = std::string(buff);
-#endif
-	delete[] buff;
-
-	// this presumes that a VS Post-Build step copies the result of the build to $(SolutionDir)\bin
-	// it is further presumed that ALL OS specific build procedures will do the same.
-	// (also works if project's Debug property "working directory" is set to $(SolutionDir))
-	// So we got that going for us.
-	pathToAssets = currentWorkingDir.substr(0, currentWorkingDir.rfind("\\bin"));
-}
 
 void error_callback(int error, const char *description) {
 	fprintf(stderr, "Error: %s\n", description);
@@ -65,14 +40,9 @@ static void cursor_position_callback(GLFWwindow *window, double x, double y) {
 	if (pxgl != NULL)
 		pxgl->MouseEvent((int)x, (int)y, state);
 }
-static void window_size_callback(GLFWwindow *window, int width, int height) {
-	if (pxgl != NULL)
-		pxgl->Reshape(width, height);
-}
+
 int main(void) {
 	GLFWwindow *window;
-	int width, height;
-
 	/*
 	if (!FreeConsole()) {
 	printf("Freeing the console failed: %d\n", GetLastError());
@@ -101,7 +71,7 @@ int main(void) {
 
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetCursorPosCallback(window, cursor_position_callback);
-	glfwSetWindowSizeCallback(window, window_size_callback);
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	glfwSwapInterval(1);
 
@@ -112,23 +82,28 @@ int main(void) {
 		exit(-1);
 	}
 
-	glfwGetFramebufferSize(window, &width, &height);
-
-	SetGlobalWorkingDirectoryName();
-	pathToAssets = currentWorkingDir + "\\..";
+	pathToAssets = "..";
 
 	try {
 		pxgl = new PhysXXGL();
-		pxgl->Reshape(width, height);
 
 		while (!glfwWindowShouldClose(window)) {
-			glfwPollEvents();
+			int width, height;
+			static int prevWidth = 0, prevHeight = 0;
 
-			glfwSwapBuffers(window);
+			glfwGetFramebufferSize(window, &width, &height);
+			if ((width != prevWidth) || (height != prevHeight)){
+				pxgl->Reshape(width, height);
+				prevWidth = width;
+				prevHeight = height;
+			}
 
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			pxgl->Display();
+
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
 
 		glfwTerminate();
