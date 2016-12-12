9a91418a04d35691d862f3f0d9b392d078e48f23
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-12-12 21:06:56 +0000

    Added XGLGuiLabel class as an experiment. Now to move it into xglgui.cpp and xglgui.h
    XGLGuiCanvas is where text rendering nuggets ought to be, specifically including MeasureFontMetrics() method.

--- examples/GUIDev/GUIDevBuildScene.cpp
+++ examples/GUIDev/GUIDevBuildScene.cpp
@@ -11,13 +11,55 @@
 **************************************************************/
 #include "ExampleXGL.h"
 
+class XGLGuiLabel : public XGLGuiCanvas {
+public:
+	XGLGuiLabel(XGL *xgl, std::string name, int x, int y) : XGLGuiCanvas(xgl) {
+		SetName(name);
+		model = glm::translate(glm::mat4(), glm::vec3(x, y, 0.0));
+		attributes.ambientColor = { 1, 1, 1, 0.1 };
+
+		// twiddle the layout variables according to an arbitrarilly chosen font size
+		MeasureFontMetrics(name);
+
+		// the actual label is a child object, because we need to measure the size of the
+		// required text bounding box first, and setting the geometry shape inside the
+		// constructor of a shape is problematic. (The "shader" member is not yet set,
+		// therefore the "programId" isn't known yet, so OpenGL calls break)
+		// This is a workaround, I'm willing to incur the technical debt for now.
+		AddChildShape("shaders/ortho-tex", [xgl, this]() { label = new XGLGuiCanvas(xgl, labelWidth, labelHeight); return label; });
+		label->SetName("Label", false);
+		label->attributes.diffuseColor = white;
+		label->attributes.ambientColor = { 1, 1, 1, 0.1 };
+		label->SetPenPosition(labelPadding / 2, labelHeight - (baselineHeight + (labelPadding / 2)));
+		label->RenderText(name.c_str(), pixelSize);
+	};
+
+	void MeasureFontMetrics(std::string name){
+		font.SetPixelSize(pixelSize);
+		fontHeight = font.MeasureFontHeight();
+		baselineHeight = font.MeasureBaselineHeight();
+		labelPadding = pixelSize * 2 / 3;
+		labelWidth = font.MeasureStringWidth(name) + labelPadding;
+		labelHeight = fontHeight + labelPadding;
+	};
+
+private:
+	XGLGuiCanvas *label;
+	int fontHeight, baselineHeight, labelPadding, labelWidth, labelHeight;
+	static const int pixelSize = 12;
+};
+
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
+	XGLGuiManager *gm = GetGuiManager();
 	XGLGuiSlider *hs;
+	XGLGuiLabel *gl;
 
 	AddShape("shaders/000-simple", [&](){ shape = new XGLTriangle(); return shape; });
 
-	XGLGuiCanvas *sliders = (XGLGuiCanvas *)(GetGuiManager()->FindObject("HorizontalSliderWindow"));
+	gm->AddChildShape("shaders/ortho-tex", [&gl, this]() { gl = new XGLGuiLabel(this, "Test XGLGuiLabel", 600, 20); return gl; });
+
+	XGLGuiCanvas *sliders = (XGLGuiCanvas *)(gm->FindObject("HorizontalSliderWindow"));
 	if (sliders != nullptr) {
 		if ((hs = (XGLGuiSlider *)sliders->FindObject("Horizontal Slider 1")) != nullptr) {
 			hs->AddMouseEventListener([hs](float x, float y, int flags) {
