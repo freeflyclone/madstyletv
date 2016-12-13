272383af9bee4e0ac2d60261955e9d42e7be046c
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-12-13 07:55:24 +0000

    Added XGLGuiLabel to XGLGuiTextEdit, a la XGLGuiSlider.

--- examples/GUIDev/GUIDevBuildScene.cpp
+++ examples/GUIDev/GUIDevBuildScene.cpp
@@ -15,30 +15,37 @@ class XGLGuiTextEdit : public XGLGuiCanvas {
 public:
 	XGLGuiTextEdit(XGL *xgl, std::string name, int x, int y, int w, int h) : XGLGuiCanvas(xgl, w, h){
 		SetName(name);
-		attributes.ambientColor = {0.00001, 0.00001, 0.00001, 0.8};
+		attributes.ambientColor = XGLColors::black;
+		attributes.ambientColor.a = 0.7f;
 		attributes.diffuseColor = XGLColors::black;
-		model = glm::translate(glm::mat4(), glm::vec3(x, y, 0));
+
+		AddChildShape("shaders/ortho", [xgl, this, name, x, y]() { label = new XGLGuiLabel(xgl, name, x, y); return label; });
+		label->model = glm::translate(glm::mat4(), glm::vec3(-(label->labelWidth + label->labelPadding), 0, 0.0));
+
+		model = glm::translate(glm::mat4(), glm::vec3(x + (label->labelWidth + label->labelPadding), y, 0));
 	}
 
 private:
-	XGLGuiCanvas *label;
+	XGLGuiLabel *label;
 };
 
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
 	XGLGuiManager *gm = GetGuiManager();
 	XGLGuiWindow *gw;
-	XGLGuiSlider *hs;
-	XGLGuiLabel *gl;
 	XGLGuiTextEdit *gte;
 
 	AddShape("shaders/000-simple", [&](){ shape = new XGLTriangle(); return shape; });
 
 	gm->AddChildShape("shaders/ortho-tex", [&]() { gw = new XGLGuiWindow(this, "TextWindow", 480, 20, 360, 200); return gw; });
-	gw->attributes.diffuseColor = XGLColors::yellow;
+	gw->attributes.diffuseColor = { 0.7, 0.7, 0.7, 1.0 };
+	gw->attributes.ambientColor = { 0.1, 0.1, 0.1, 0.5 };
+
 	gw->SetPenPosition(10, 20);
 	gw->RenderText("Container for XGLGuiTextEdit fields.\n", 20);
 
-	gw->AddChildShape("shaders/ortho-tex", [&gl, this]() { gl = new XGLGuiLabel(this, "Test XGLGuiLabel", 10, 40); return gl; });
-	gw->AddChildShape("shaders/ortho-tex", [&gte, this]() { gte = new XGLGuiTextEdit(this, "", 140, 40, 200, 24); return gte; });
+	gw->AddChildShape("shaders/ortho-tex", [&gte, this]() { gte = new XGLGuiTextEdit(this, "Text Edit 1", 20, 40, 200, 24); return gte; });
+	gw->AddChildShape("shaders/ortho-tex", [&gte, this]() { gte = new XGLGuiTextEdit(this, "Text Edit 2", 20, 80, 200, 24); return gte; });
+	gw->AddChildShape("shaders/ortho-tex", [&gte, this]() { gte = new XGLGuiTextEdit(this, "Text Edit 3", 20, 120, 200, 24); return gte; });
+	gw->AddChildShape("shaders/ortho-tex", [&gte, this]() { gte = new XGLGuiTextEdit(this, "Text Edit 4", 20, 160, 200, 24); return gte; });
 }
