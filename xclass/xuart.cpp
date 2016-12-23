69f9de439b70a19738e190357805a101eb174148
evan <evan-github@madstyle.net>
2016-12-23 19:00:23 +0000

    Added implementation file for XUart Added mavlink.props, bringing in the MAVLink protocol headers Twiddled XUart to be just a HAL layer for a serial port, ie: removed the XThread base class. Created XMavlink class in GUIDevBuildScene.cpp which derives from XUart and XThread.

---
+++ xclass/xuart.cpp
@@ -0,0 +1,53 @@
+#include "xuart.h"
+
+XUart::XUart(std::string portName) : hPort(INVALID_HANDLE_VALUE) {
+	// Windows needs a wide string if we're configured for Unicode, and we are.
+	std::wstringstream ws;
+	ws << portName.c_str();
+	std::wstring name = ws.str();
+
+	if ((hPort = CreateFile(name.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) == INVALID_HANDLE_VALUE)
+		throw std::runtime_error("failed to open serial port: " + portName);
+
+	portDCB.DCBlength = sizeof(DCB);
+	GetCommState(hPort, &portDCB);
+
+	portDCB.fBinary = TRUE;                         // Binary mode; no EOF check 
+	portDCB.fParity = TRUE;                         // Enable parity checking  
+	portDCB.fDsrSensitivity = FALSE;                // DSR sensitivity  
+	portDCB.fErrorChar = FALSE;                     // Disable error replacement  
+	portDCB.fOutxDsrFlow = FALSE;                   // No DSR output flow control  
+	portDCB.fAbortOnError = FALSE;                  // Do not abort reads/writes on error 
+	portDCB.fNull = FALSE;                          // Disable null stripping  
+	portDCB.fTXContinueOnXoff = TRUE;               // XOFF continues Tx  
+	portDCB.BaudRate = 115200;
+	portDCB.Parity = NOPARITY;
+	portDCB.StopBits = ONESTOPBIT;
+
+	if (!SetCommState(hPort, &portDCB))
+		throw std::runtime_error("SetCommState() failed");
+
+	commTimeouts.ReadIntervalTimeout = 50;
+	commTimeouts.ReadTotalTimeoutConstant = 50;
+	commTimeouts.ReadTotalTimeoutMultiplier = 10;
+	commTimeouts.WriteTotalTimeoutMultiplier = 10;
+	commTimeouts.WriteTotalTimeoutConstant = 50;
+
+	if (!SetCommTimeouts(hPort, &commTimeouts))
+		throw std::runtime_error("SetCommTimeouts() failed");
+};
+
+XUart::~XUart() {
+	xprintf("XUart::~XUart()\n");
+	if (hPort != INVALID_HANDLE_VALUE)
+		CloseHandle(hPort);
+};
+
+int XUart::Read(unsigned char *b, int size) {
+	int nRead = 0;
+	if (ReadFile(hPort, b, size, (LPDWORD)(&nRead), NULL))
+		return nRead;
+	else {
+		return 0;
+	}
+}
--- xclass/xuart.h
+++ xclass/xuart.h
@@ -2,72 +2,22 @@
 #define XUART_H
 
 #include <string>
+#include <sstream>
+#include <Windows.h>
+#include "xutils.h"
 
-
-class XUart : XThread {
+class XUart {
 public:
-	XUart(std::string portName) : hPort(INVALID_HANDLE_VALUE), XThread(portName+"Thread") {
-		std::wstringstream ws;
-		ws << portName.c_str();
-		std::wstring name = ws.str();
+	XUart(std::string portName);
 
-		if ( (hPort = CreateFile(name.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) == INVALID_HANDLE_VALUE)
-			throw std::runtime_error("failed to open serial port: " + portName);
+	~XUart();
+	int Read(unsigned char *b, int size);
 
-		portDCB.DCBlength = sizeof(DCB);
-		GetCommState(hPort, &portDCB);
-
-		portDCB.fBinary = TRUE;                         // Binary mode; no EOF check 
-		portDCB.fParity = TRUE;                         // Enable parity checking  
-		portDCB.fDsrSensitivity = FALSE;                // DSR sensitivity  
-		portDCB.fErrorChar = FALSE;                     // Disable error replacement  
-		portDCB.fOutxDsrFlow = FALSE;                   // No DSR output flow control  
-		portDCB.fAbortOnError = FALSE;                  // Do not abort reads/writes on error 
-		portDCB.fNull = FALSE;                          // Disable null stripping  
-		portDCB.fTXContinueOnXoff = TRUE;               // XOFF continues Tx  
-		portDCB.BaudRate = 115200;
-		portDCB.Parity = NOPARITY;
-		portDCB.StopBits = ONESTOPBIT;
-
-		if (!SetCommState(hPort, &portDCB))
-			throw std::runtime_error("SetCommState() failed");
-
-		commTimeouts.ReadIntervalTimeout = 50;
-		commTimeouts.ReadTotalTimeoutConstant = 50;
-		commTimeouts.ReadTotalTimeoutMultiplier = 10;
-		commTimeouts.WriteTotalTimeoutMultiplier = 10;
-		commTimeouts.WriteTotalTimeoutConstant = 50;
-
-		if (!SetCommTimeouts(hPort, &commTimeouts))
-			throw std::runtime_error("SetCommTimeouts() failed");
-
-		Start();
-	};
-
-	~XUart(){
-		Stop();
-
-		xprintf("XUart::~XUart()\n");
-		if (hPort != INVALID_HANDLE_VALUE)
-			CloseHandle(hPort);
-	};
-
-	void Run() {
-		while (IsRunning()) {
-			nRead = 0;
-			if (ReadFile(hPort, buffer, sizeof(buffer), (LPDWORD)(&nRead), NULL)) {
-				xprintf("Read %d bytes\n", nRead);
-			}
-			else
-				std::this_thread::sleep_for(std::chrono::duration<int, std::milli>(1));
-		}
-	}
-
+private:
 	HANDLE hPort;
 	DCB portDCB;
 	COMMTIMEOUTS   commTimeouts;
 	unsigned char buffer[1024];
-	unsigned int nRead;
 };
 
 #endif
