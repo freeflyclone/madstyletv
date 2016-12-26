09592a58da5b655558b5c1ce9e09579d4faa81c5
evan <evan@919ba797-19bd-4c7d-b351-8ae5bbea26e1>
2016-12-26 06:02:46 +0000

    Fleshing out XMavlink class...
    XMavlink defines 2 (for now) nested classes: ReadThread and WriteThread.
    
    XMavlink asynchronously manages a communication channel with a MAV.  Multiple instances of XMavlink are possible.  Currently via serial port, but any network medium is possible, given MAVLINK's byte oriented structure.

--- examples/MavlinkDev/xmavlink.h
+++ examples/MavlinkDev/xmavlink.h
@@ -5,38 +5,47 @@
 #include "xuart.h"
 #include "ardupilotmega/mavlink.h"
 
-class XMavlink : public XUart {
-public:
-	class ReadThread : public XThread {
-	public:
-		ReadThread(XUart *px);
-		~ReadThread();
-		void Run();
-		bool MessageDump(mavlink_message_t msg);
-
-	private:
-		unsigned char cp, parseState;
-		XUart *pXUart;
-		mavlink_message_t msg;
-		mavlink_status_t stat;
-	};
-
-	XMavlink(std::string portName);
-	~XMavlink();
-
-	ReadThread *rxThread;
-
-	unsigned char cp;
-	unsigned char buffer[512];
+struct XMavlinkMessages {
 	mavlink_message_t msg;
 	mavlink_status_t stat;
 	mavlink_attitude_t attitude;
 	mavlink_sys_status_t sysStatus;
 	mavlink_vfr_hud_t vfrHud;
 	mavlink_statustext_t statusText;
+};
 
-	unsigned char parseState;
+class XMavlink : public XUart {
+public:
+	class ReadThread : public XThread {
+	public:
+		ReadThread(XMavlink *);
+		~ReadThread();
+		void Run();
+		bool MessageDump();
 
+	private:
+		unsigned char cp, parseState;
+		XMavlink *pMavlink;
+	};
+
+	class WriteThread : public XThread {
+	public:
+		WriteThread(XMavlink *);
+		~WriteThread();
+		void Run();
+		bool WriteMessage(mavlink_message_t);
+
+	private:
+		XMavlink *pMavlink;
+	};
+
+	XMavlink(std::string portName);
+	~XMavlink();
+
+	ReadThread *rThread;
+	WriteThread *wThread;
+
+	XMavlinkMessages messages;
 };
 
 
