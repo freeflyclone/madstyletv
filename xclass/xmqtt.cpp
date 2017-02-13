67a683e5ec486415fb51af3b14919145f75448ed
Evan Mortimore <evan-github@madstyle.net>
2017-02-12 19:11:41 -0800

    First signs of life with Mosquitto...
    Added LoopThread to XMqtt, and the necessary Mosquitto
    library calls to initialize a connection and register
    a couple of callback handlers.

--- xclass/xmqtt.cpp
+++ xclass/xmqtt.cpp
@@ -1,10 +1,54 @@
 #include "xmqtt.h"
 
-XMqtt::XMqtt() {
+XMqtt::LoopThread::LoopThread(XMqtt &pm) : XThread("XMqtt::LoopThread"), pMqtt(pm) {
+    Start();
+}
+
+XMqtt::LoopThread::~LoopThread() {
+    Stop();
+}
+
+void XMqtt::LoopThread::Run() {
+    while (IsRunning()) {
+        mosquitto_loop(pMqtt.mosq, -1, 1);
+    }
+}
+
+XMqtt::XMqtt(std::string h, int p) : host(h), port(p), loopThread(NULL) {
+    SetName("XMqtt");
+
 	mosquitto_lib_init();
+
+    if (!(mosq = mosquitto_new(XObject::Name().c_str(), true, this)))
+        throw std::runtime_error("Failed creating a new mosquitto instance" + Name());
+
+    mosquitto_connect_callback_set(mosq, ConnectCallback);
+    mosquitto_message_callback_set(mosq, MessageCallback);
+
+    if (mosquitto_connect(mosq, host.c_str(), port, 10) != MOSQ_ERR_SUCCESS)
+        throw std::runtime_error("Failed to connect to host '" + host + "':" + std::to_string(port));
+
+    if (mosquitto_subscribe(mosq, NULL, "test/topic", 0) != MOSQ_ERR_SUCCESS)
+        throw std::runtime_error("Failed to subscribe to 'test/topic'");
+
+    loopThread = new LoopThread(*this);
 }
 
 XMqtt::~XMqtt() {
+}
+
+void XMqtt::ConnectCallback(Mosquitto mosq, void *obj, int result) {
+    XMqtt *p = (XMqtt *)obj;
+
+    xprintf("ConnectCallback(): %s, rc=%d\n", p->Name().c_str(), result);
+    // do a Listener callback here
+}
+
+void XMqtt::MessageCallback(Mosquitto mosq, void *obj, Message msg) {
+    XMqtt *p = (XMqtt *)obj;
+
+    xprintf("MessageCallback(): '%.*s' for topic '%s'\n", msg->payloadlen, (char *)msg->payload, msg->topic);
+    // do a Listener callback here
 }
 
 int XMqtt::Read(unsigned char *b, int size) {
--- xclass/xmqtt.h
+++ xclass/xmqtt.h
@@ -6,17 +6,38 @@
 #include <stdexcept>
 #include <mosquitto.h>
 #include "xobject.h"
+#include "xthread.h"
 #include "xutils.h"
 
 class XMqtt : public XObject {
 public: 
-	XMqtt();
+    typedef mosquitto *Mosquitto;
+    typedef const mosquitto_message *Message;
+
+    class LoopThread : public XThread {
+    public:
+        LoopThread(XMqtt &);
+        ~LoopThread();
+        void Run();
+
+    private:
+        XMqtt &pMqtt;
+    };
+
+    XMqtt(std::string h="localhost", int p=1883);
 	~XMqtt();
+
+    static void ConnectCallback(Mosquitto, void *, int);
+    static void MessageCallback(Mosquitto, void *, Message);
 
 	int Read(unsigned char *b, int size);
 	int Write(unsigned char *b, int size);
 
 private:
+    std::string host;
+    int port;
+    Mosquitto mosq;
+    LoopThread *loopThread;
 };
 
 #endif // XMQTT_H
