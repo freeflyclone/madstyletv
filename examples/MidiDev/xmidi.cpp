4f6c8e3d205450bd8a80bc51875d7eb9c44ba40f
Evan Mortimore <evan-git@madstyle.net>
2017-05-21 14:14:51 -0700

    Cleanup of xmidi.cpp prior to Linux version.

--- examples/MidiDev/xmidi.cpp
+++ examples/MidiDev/xmidi.cpp
@@ -5,67 +5,57 @@ void CALLBACK MidiInProc(HMIDIIN hMidiIn, UINT wMsg, DWORD_PTR dwInstance, DWORD
 	int key;
 	switch (wMsg) {
 	case MIM_OPEN:
-		xprintf("MidiInProc(): Open\n");
+		//xprintf("MidiInProc(): Open\n");
 		break;
 
 	case MIM_CLOSE:
-		xprintf("MidiInProc(): Close\n");
+		//xprintf("MidiInProc(): Close\n");
 		break;
 
 	case MIM_DATA:
 		key = LOBYTE(LOWORD(dwParam1)) << 8 | HIBYTE(LOWORD(dwParam1));
-		//xprintf("MidiInProc(): Data %02X %02X %02X, (%04X) %d\n", LOBYTE(LOWORD(dwParam1)), HIBYTE(LOWORD(dwParam1)), LOBYTE(HIWORD(dwParam1)), key, dwParam2);
 		pmi->KeyEvent(key, LOBYTE(HIWORD(dwParam1)));
 		break;
 
 	case MIM_LONGDATA:
-		xprintf("MidiInProc(): LongData\n");
+		//xprintf("MidiInProc(): LongData\n");
 		break;
 
 	case MIM_ERROR:
-		xprintf("MidiInProc(): Error\n");
+		//xprintf("MidiInProc(): Error\n");
 		break;
 
 	case MIM_LONGERROR:
-		xprintf("MidiInProc(): LongError\n");
+		//xprintf("MidiInProc(): LongError\n");
 		break;
 	}
 }
 
-
 XMidiInput::XMidiInput(std::wstring devName) : deviceName(devName), XThread("XMidiInput::ReadThread") {
-	xprintf("XMidiInput::XMidiInput()\n");
-
 	unsigned int numMidiIn = midiInGetNumDevs();
 	MIDIINCAPS caps;
-	xprintf("Found %d MIDI devices\n", numMidiIn);
 
+	// build a map, by name, of MIDI devices and the OS related stuff needed
+	// to interact with each of them.
 	for (unsigned int i = 0; i < numMidiIn; i++) {
 		midiInGetDevCaps(i, &caps, sizeof(caps));
 		deviceList[caps.szPname] = { i, caps };
 	}
-
-	xprintf("Enumerated %d devices\n", deviceList.size());
-	for (auto md : deviceList) {
-		xprintf("Device %S, idx: %d\n", md.second.second.szPname, md.second.first);
-	}
 }
 
-XMidiInput::~XMidiInput() {
-	xprintf("XMidiInput::~XMidiInput()\n");
-}
+XMidiInput::~XMidiInput() {}
 
 void XMidiInput::Open() {
 	MidiInIterator i;
 
-	xprintf("Looking for: '%S'\n", deviceName.c_str());
+	xprintf("Looking for: '%S'... ", deviceName.c_str());
 
 	if ((i = deviceList.find(deviceName)) == deviceList.end()) {
 		xprintf("no device with that name\n");
 		return;
 	}
 
-	xprintf("Found it: ", i->second.second.szPname);
+	xprintf("found it!\n");
 
 	if (midiInOpen(&hMidiIn, i->second.first, (DWORD_PTR)MidiInProc, (DWORD_PTR)this, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) {
 		xprintf("device open failed\n");
