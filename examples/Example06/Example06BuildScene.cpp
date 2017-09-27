a8914bb9b18529dc0f2e577de8ee35c314cb101f
Evan Mortimore <evan-git@madstyle.net>
2017-09-26 21:22:38 -0700

    Refactored Exampl06, now has XGLSled...
    ... for orientation visualization.

--- examples/Example06/Example06BuildScene.cpp
+++ examples/Example06/Example06BuildScene.cpp
@@ -1,65 +1,76 @@
 /**************************************************************
 ** Example06BuildScene.cpp
 **
-** Just to demonstrate instantiation of a "ground"
-** plane and a single triangle, with default camera manipulation
-** via keyboard and mouse, and joystick movement of the triangle
-** around the ground plane with the first 2 axes of the first
-** joystick in the system.
+** Experiments with XGLSled...
+**
+** XGLSled is an XGLShape with some aircraft-style attitude
+** control methods.
 **************************************************************/
 #include "ExampleXGL.h"
 
-XGLShape *triangle, *cube, *xAxis,*yAxis,*zAxis;
+class XGLSled : public XGLShape {
+public:
+	XGLSled() : yaw(0.0f), pitch(0.0f), pos(10.0f, 0.0f, 0.0f) { 
+		SetName("XGLSled");
 
-float yaw = 0.0f, pitch = 0.0f;
-float x = 10.0f;
-float y = 0.0f;
-float z = 0.0f;
+		// 3 lines to represent X,Y,Z axes (orientation)
+		// X
+		v.push_back({ glm::vec3(0), {}, {}, XGLColors::red });
+		v.push_back({ glm::vec3(1.0, 0.0, 0.0) * 5.0f, {}, {}, XGLColors::red });
+		// Y
+		v.push_back({ glm::vec3(0), {}, {}, XGLColors::green });
+		v.push_back({ glm::vec3(0.0, 1.0, 0.0) * 5.0f, {}, {}, XGLColors::green });
+		// Z
+		v.push_back({ glm::vec3(0), {}, {}, XGLColors::blue });
+		v.push_back({ glm::vec3(0.0, 0.0, 1.0) * 5.0f, {}, {}, XGLColors::blue });
+	}
 
-glm::vec3 pos = { 10.0, 0.0, 0.0 };
-glm::vec3 up = { 0.0, 0.0, 1.0 };
-glm::vec3 front;
+	void Draw() {
+		glDrawArrays(GL_LINES, 0, GLsizei(v.size()));
+		GL_CHECK("glDrawArrays() failed");
+	}
 
-float yawAngle, pitchAngle;
+	glm::mat4 GetOrientation() {
+		glm::quat q;
 
-glm::mat4 GetOrientation()
-{
-	glm::quat q = glm::angleAxis(glm::radians(-pitchAngle), glm::vec3(1, 0, 0));
-	q *= glm::angleAxis(glm::radians(-yawAngle), glm::vec3(0, 0, 1));
-	return glm::mat4_cast(q);
-}
+		q *= glm::angleAxis(glm::radians(-pitch), glm::vec3(1, 0, 0));
+		q *= glm::angleAxis(glm::radians(-yaw), glm::vec3(0, 0, 1));
 
-void TwiddleCube() {
-	glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(x, y, z));
-	glm::mat4 scale = glm::scale(glm::mat4(), glm::vec3(1.0, 2.0, 0.01));
-	cube->model = translate * GetOrientation();// *scale;
-}
+		return glm::mat4_cast(q);
+	}
+
+	void Twiddle(float deltaYaw, float deltaPitch) {
+		yaw += deltaYaw;
+		pitch += deltaPitch;
+
+		glm::mat4 translate = glm::translate(glm::mat4(), pos);
+		model = translate * GetOrientation();
+	}
+
+private:
+	float yaw, pitch;
+	XGLVertex pos;
+	XGLAxis *xAxis, *yAxis, *zAxis;
+};
+
+XGLShape *triangle, *cube;
+XGLSled *sled;
 
 void ExampleXGL::BuildScene() {
-	front = glm::normalize(pos * -1.0f);
-
-	// Add new triangle and cube to the scene.
+	// Add new triangle and sled to the scene.
 	AddShape("shaders/000-simple", [&](){ triangle = new XGLTriangle(); return triangle; });
-	AddShape("shaders/diffuse", [&](){ cube = new XGLCube(); return cube; });
+	AddShape("shaders/000-simple", [&](){ sled = new XGLSled(); return sled; });
 
-	// A 3-axis set of lines, to help with orientation visualization
-	CreateShape("shaders/000-simple", [&](){ xAxis = new XGLAxis(5.0f, XGLColors::red, { 1.0, 0.0, 0.0 }); return xAxis; });
-	CreateShape("shaders/000-simple", [&](){ yAxis = new XGLAxis(5.0f, XGLColors::green, { 0.0, 1.0, 0.0 }); return yAxis; });
-	CreateShape("shaders/000-simple", [&](){ zAxis = new XGLAxis(5.0f, XGLColors::blue, { 0.0, 0.0, 1.0 }); return zAxis; });
-
-	// Add 'em to the cube
-	cube->AddChild(xAxis);
-	cube->AddChild(yAxis);
-	cube->AddChild(zAxis);
-
-	// move the cube away from the triangle
-	cube->model *= glm::translate(glm::mat4(), glm::vec3(0.0, 10.0, 0.0));
+	// Add cube as child of sled, but shape it like a flat rectangle
+	CreateShape("shaders/diffuse", [&](){ cube = new XGLCube(); return cube; });
+	cube->model = glm::scale(glm::mat4(), glm::vec3(1.0, 2.0, 0.01));
+	sled->AddChild(cube);
 
 	// if an Xbox 360 controller is attached, use the left joystick X & Y axes to move triangle
 	AddProportionalFunc("Xbox360Controller0", [this](float v) { triangle->model[3][0] = v * 10.0f; });
 	AddProportionalFunc("Xbox360Controller1", [this](float v) { triangle->model[3][1] = v * 10.0f; });
 
 	// use the right stick to control orientation of the cube
-	AddProportionalFunc("Xbox360Controller2", [this](float v) { yaw = v; yawAngle += v*2.0; TwiddleCube(); });
-	AddProportionalFunc("Xbox360Controller3", [this](float v) { pitch = v; pitchAngle += v*2.0; TwiddleCube(); });
+	AddProportionalFunc("Xbox360Controller2", [this](float v) { sled->Twiddle(v*2.0, 0.0f); });
+	AddProportionalFunc("Xbox360Controller3", [this](float v) { sled->Twiddle(0.0f, v*2.0f); });
 }
