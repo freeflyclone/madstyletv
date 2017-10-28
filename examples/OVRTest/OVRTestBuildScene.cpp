c391fdcca4ab9b01440c56720c595652e2b2846f
Evan Mortimore <evan-git@madstyle.net>
2017-10-27 20:19:17 -0700

    Got flight controls working!!!...
    Forward/Backward is as James suggested: pick a "forward" vector and multiply it by the orientation, then
    add it to the rootShape's position.
    
    Lighting is borked now, need XGLShape-based objects of some sort.

--- examples/OVRTest/OVRTestBuildScene.cpp
+++ examples/OVRTest/OVRTestBuildScene.cpp
@@ -26,66 +26,76 @@ float speed3 = constSpeed3;
 // constructed in OVRTestGUI.cpp / ExampleXGL::BuildGui()
 extern XGLGuiManager *appGuiManager;
 
+const XGLColor gray = { 0.005, 0.005, 0.005, 1 };
+
 void ExampleXGL::BuildScene() {
-	XGLShape *shape, *child1, *child2, *child3, *child4;
+	XGLShape *shape;
 	glm::mat4 rotate, translate;
 
-	CreateShape("shaders/specular", [&](){ shape = new XGLTorus(5.0f, 1.0f, 64, 32); return shape; });
-	shape->attributes.diffuseColor = { 0.005, 0.005, 0.005, 1 };
-	shape->SetAnimationFunction([shape](float clock) {
-		glm::mat4 rotate = glm::rotate(glm::mat4(), clock / speed1, glm::vec3(1.0f, 0.0f, 0.0f));
-		shape->model = rotate;
-	});
-	CreateShape("shaders/specular", [&](){ child3 = new XGLTransformer(); return child3; });
-	child3->SetAnimationFunction([child3](float clock) {
-		float translateFunction = sin(clock / 180.0f);
-		glm::mat4 rotate = glm::rotate(glm::mat4(), clock / speed2, glm::vec3(0.0f, 0.0f, 1.0f));
-		child3->model = rotate;
-	});
-	rootShape->AddChild(shape);
-	shape->AddChild(child3);
+	// create spinny torus thingy...
+	if (true) {
+		XGLShape *bigGrayTorus;
+		CreateShape("shaders/specular", [&](){ bigGrayTorus = new XGLTorus(5.0f, 1.0f, 64, 32); return bigGrayTorus; });
+		bigGrayTorus->attributes.diffuseColor = gray;
+		bigGrayTorus->SetAnimationFunction([bigGrayTorus](float clock) {
+			glm::mat4 rotate = glm::rotate(glm::mat4(), clock / speed1, glm::vec3(1.0f, 0.0f, 0.0f));
+			bigGrayTorus->model = rotate;
+		});
 
-	CreateShape("shaders/specular", [&](){ child4 = new XGLTorus(2.0f, 0.5f, 64, 32); return child4; });
-	child4->attributes.diffuseColor = { 1.0, 0.00001, 0.00001, 1 };
-	translate = glm::translate(glm::mat4(), glm::vec3(5.0, 0, 0));
-	rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	child4->model = translate * rotate;
+		XGLShape *bgtChildTransformer;
+		CreateShape("shaders/specular", [&](){ bgtChildTransformer = new XGLTransformer(); return bgtChildTransformer; });
+		bgtChildTransformer->SetAnimationFunction([bgtChildTransformer](float clock) {
+			float translateFunction = sin(clock / 180.0f);
+			glm::mat4 rotate = glm::rotate(glm::mat4(), clock / speed2, glm::vec3(0.0f, 0.0f, 1.0f));
+			bgtChildTransformer->model = rotate;
+		});
+		rootShape->AddChild(bigGrayTorus);
+		bigGrayTorus->AddChild(bgtChildTransformer);
 
-	CreateShape("shaders/000-simple", [&](){ child1 = new XGLTransformer(); return child1; });
-	child1->SetAnimationFunction([child1](float clock) {
-		float translateFunction = sin(clock / 180.0f);
-		glm::mat4 rotate = glm::rotate(glm::mat4(), clock / speed3, glm::vec3(0.0f, 0.0f, 1.0f));
-		child1->model = rotate;
-	});
+		XGLShape *bgtChildRedTorus;
+		CreateShape("shaders/specular", [&](){ bgtChildRedTorus = new XGLTorus(2.0f, 0.5f, 64, 32); return bgtChildRedTorus; });
+		bgtChildRedTorus->attributes.diffuseColor = XGLColors::red;
+		translate = glm::translate(glm::mat4(), glm::vec3(5.0, 0, 0));
+		rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		bgtChildRedTorus->model = translate * rotate;
+		bgtChildTransformer->AddChild(bgtChildRedTorus);
 
-	CreateShape("shaders/specular", [&](){ child2 = new XGLTorus(0.75f, 0.25f, 64, 32); return child2; });
-	child2->attributes.diffuseColor = (XGLColors::yellow);
-	translate = glm::translate(glm::mat4(), glm::vec3(2.0, 0, 0));
-	rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	child2->model = translate * rotate;
+		XGLShape *bgtCrtTransformer;
+		CreateShape("shaders/000-simple", [&](){ bgtCrtTransformer = new XGLTransformer(); return bgtCrtTransformer; });
+		bgtCrtTransformer->SetAnimationFunction([bgtCrtTransformer](float clock) {
+			float translateFunction = sin(clock / 180.0f);
+			glm::mat4 rotate = glm::rotate(glm::mat4(), clock / speed3, glm::vec3(0.0f, 0.0f, 1.0f));
+			bgtCrtTransformer->model = rotate;
+		});
+		bgtChildRedTorus->AddChild(bgtCrtTransformer);
 
-	child1->AddChild(child2);
-	child4->AddChild(child1);
-	child3->AddChild(child4);
-	
+		XGLShape *bgtGrandChildYellowTorus;
+		CreateShape("shaders/specular", [&](){ bgtGrandChildYellowTorus = new XGLTorus(0.75f, 0.25f, 64, 32); return bgtGrandChildYellowTorus; });
+		bgtGrandChildYellowTorus->attributes.diffuseColor = XGLColors::yellow;
+		translate = glm::translate(glm::mat4(), glm::vec3(2.0, 0, 0));
+		rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		bgtGrandChildYellowTorus->model = translate * rotate;
+		bgtCrtTransformer->AddChild(bgtGrandChildYellowTorus);
+	}
+
 	CreateShape("shaders/specular", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
-	shape->attributes.diffuseColor = (XGLColors::blue);
+	shape->attributes.diffuseColor = XGLColors::blue;
 	shape->model = glm::translate(glm::mat4(), glm::vec3(20, 0, 0));
 	rootShape->AddChild(shape);
 
 	CreateShape("shaders/specular", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
-	shape->attributes.diffuseColor = (XGLColors::red);
-	shape->model = glm::translate(glm::mat4(), glm::vec3(-20, 0, 0))
-		* glm::scale(glm::mat4(), glm::vec3(2, 2, 2));
+	shape->attributes.diffuseColor = XGLColors::red;
+	shape->model = glm::translate(glm::mat4(), glm::vec3(-20, 0, 0)) * glm::scale(glm::mat4(), glm::vec3(2, 2, 2));
 	rootShape->AddChild(shape);
 
 	CreateShape("shaders/specular", [&](){ shape = new XGLTorus(3.0f, 0.5f, 64, 32); return shape; });
-	shape->attributes.diffuseColor = (XGLColors::green);
+	shape->attributes.diffuseColor = XGLColors::green;
 	shape->model = glm::translate(glm::mat4(), glm::vec3(30, 0, 0))
 		* glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(0, 1, 0))
 		* glm::scale(glm::mat4(), glm::vec3(2, 2, 2));
 	rootShape->AddChild(shape);
 
+	// function to toggle wire frame rendering
 	XInputKeyFunc renderMod = [&](int key, int flags) {
 		const bool isDown = (flags & 0x8000) == 0;
 		const bool isRepeat = (flags & 0x4000) != 0;
@@ -98,70 +108,67 @@ void ExampleXGL::BuildScene() {
 		}
 	};
 
+	// add the function to M key handler, both upper and lower case.
 	AddKeyFunc('M', renderMod);
 	AddKeyFunc('m', renderMod);
 
-	// HMD "sled" provides a single anchor point in world space for the HMD and Touch controllers
-	// and anything else that needs to rendered as part of the user's "personal space".
-	//
-	// Could be a car interior, an aircraft/spacecraft cockpit, or what have you.
-	//
-	// NOTE: the way it works now, only "top-level" objects (created by AddShape()) will be
-	//       affected by the optional "layer" argument.  All child objects thereof will
-	//       rendered at that object's layer's time.
-	/*
-	AddShape("shaders/000-simple", [&]() {hmdSled = new XGLTransformer(); return hmdSled; }, 2);
-	hmdSled->SetName("HmdSled");
-	hmdSled->SetAnimationFunction([&](float clock) {
-		glm::mat4 t = glm::translate(glm::mat4(), hmdSled->p);
-		hmdSled->model = t;
-	});
-	*/
-/*
-	AddShape("shaders/000-simple", [&]() {hmdSled = new XGLSled(); return hmdSled; }, 2);
-	hmdSled->SetName("HmdSled");
-
-	CreateShape("shaders/specular", [&](){ shape = new XGLSphere(0.01f, 4); shape->SetName("SledOrigin");  return shape; }, 2);
-	hmdSled->AddChild(shape);
-	hmdSled->AddChild(appGuiManager);
-	*/
-
+	// LeftHand, anchored to the viewpoint
 	AddShape("shaders/specular", [&]() { shape = new XGLSphere(0.05f, 64); shape->SetName("LeftHand"); return shape; });
 
-	CreateShape("shaders/specular", [&]() { leftFinger = new XGLCapsule(0.01f, 0.1, 32); leftFinger->SetName("LeftFinger"); return leftFinger; });
+	// Left Finger, child of LeftHand
+	CreateShape("shaders/specular", [&]() { leftFinger = new XGLCapsule(0.01f, 0.1f, 32); leftFinger->SetName("LeftFinger"); return leftFinger; });
 	translate = glm::translate(glm::mat4(), glm::vec3(0.0, 0.0, -0.1));
 	rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(0.0, 1.0, 0.0));
 	leftFinger->model = translate * rotate;
 	shape->AddChild(leftFinger);
 
-	CreateShape("shaders/specular", [&]() { leftThumb = new XGLCapsule(0.01f, 0.075, 32); leftThumb->SetName("RightThumb"); return leftThumb; });
+	// LeftThumb, child of LeftHand
+	CreateShape("shaders/specular", [&]() { leftThumb = new XGLCapsule(0.01f, 0.075f, 32); leftThumb->SetName("LeftThumb"); return leftThumb; });
 	translate = glm::translate(glm::mat4(), glm::vec3(0.06, 0.0, -0.06));
 	rotate = glm::rotate(glm::mat4(), glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
 	leftThumb->model = translate * rotate;
 	shape->AddChild(leftThumb);
 
+
+	// RightHand, anchored to the viewpoint
 	AddShape("shaders/specular", [&]() { shape = new XGLSphere(0.05f, 64); shape->SetName("RightHand"); return shape; });
 
-	CreateShape("shaders/specular", [&]() { rightFinger = new XGLCapsule(0.01f, 0.1, 32); rightFinger->SetName("RightFinger"); return rightFinger; });
+	// RightFinger, child of RightHand
+	CreateShape("shaders/specular", [&]() { rightFinger = new XGLCapsule(0.01f, 0.1f, 32); rightFinger->SetName("RightFinger"); return rightFinger; });
 	translate = glm::translate(glm::mat4(), glm::vec3(0.0, 0.0, -0.1));
 	rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(0.0, 1.0, 0.0));
 	rightFinger->model = translate * rotate;
 	shape->AddChild(rightFinger);
 
-	CreateShape("shaders/specular", [&]() { rightThumb = new XGLCapsule(0.01f, 0.075, 32); rightThumb->SetName("RightThumb"); return rightThumb; });
+	// RightThumb, child of Right Hand
+	CreateShape("shaders/specular", [&]() { rightThumb = new XGLCapsule(0.01f, 0.075f, 32); rightThumb->SetName("RightThumb"); return rightThumb; });
 	translate = glm::translate(glm::mat4(), glm::vec3(-0.06, 0.0, -0.06));
 	rotate = glm::rotate(glm::mat4(), glm::radians(135.0f), glm::vec3(0.0, 1.0, 0.0));
 	rightThumb->model = translate * rotate;
 	shape->AddChild(rightThumb);
 
-	AddProportionalFunc("LeftThumbStick.x", [this](float v) { rootShape->p.x -= v / 10.0f; rootShape->model = rootShape->GetFinalMatrix(); });
-	AddProportionalFunc("LeftThumbStick.y", [this](float v) { rootShape->p.y -= v / 10.0f; rootShape->model = rootShape->GetFinalMatrix(); });
-	AddProportionalFunc("LeftHandTrigger", [this](float v) { rootShape->p.z += v / 10.0f; rootShape->model = rootShape->GetFinalMatrix(); });
-	AddProportionalFunc("RightHandTrigger",[this](float v) { rootShape->p.z -= v / 10.0f; rootShape->model = rootShape->GetFinalMatrix(); });
+	// move forward
+	AddProportionalFunc("LeftIndexTrigger", [this](float v) { 
+		glm::vec4 forward = glm::vec4(0.0, -v / 10.0f, 0.0, 0.0) * glm::toMat4(rootShape->o);
+		rootShape->p += glm::vec3(forward);
 
+		rootShape->model = rootShape->GetFinalMatrix(); 
+	});
+
+	// move backward
+	AddProportionalFunc("LeftHandTrigger", [this](float v) {
+		glm::vec4 backward = glm::vec4(0.0, v / 10.0f, 0.0, 0.0) * glm::toMat4(rootShape->o);
+		rootShape->p += glm::vec3(backward);
+
+		rootShape->model = rootShape->GetFinalMatrix();
+	});
+
+	// yaw (rudder)
+	AddProportionalFunc("LeftThumbStick.x", [this](float v) { rootShape->SampleInput(v, 0.0f, 0.0f); });
+
+	// pitch (elevator)
 	AddProportionalFunc("RightThumbStick.y", [this](float v) { rootShape->SampleInput(0.0f, v, 0.0f); });
-	AddProportionalFunc("RightThumbStick.x", [this](float v) { rootShape->SampleInput(v, 0.0f, 0.0f); });
 
-	AddProportionalFunc("RightIndexTrigger", [this](float v) { rootShape->SampleInput(0.0f, 0.0f, -v); });
-	AddProportionalFunc("LeftIndexTrigger", [this](float v) { rootShape->SampleInput(0.0f, 0.0f, v); });
+	// roll (ailerons)
+	AddProportionalFunc("RightThumbStick.x", [this](float v) { rootShape->SampleInput(0.0f, 0.0f, -v); });
 }
