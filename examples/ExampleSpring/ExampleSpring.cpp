ba02102beddb514af8ef5ba7655df34bf695a7b0
Evan Mortimore <evan-git@madstyle.net>
2017-11-12 19:28:07 -0800

    Reworked Spring class...
    ... it now takes 2 XGLShapes in the constructor, and operates on their "p" points.
    Added 4 balls, and 6 springs, in a tetrahedron. It settles out to 4 equilateral triangles.

--- examples/ExampleSpring/ExampleSpring.cpp
+++ examples/ExampleSpring/ExampleSpring.cpp
@@ -5,56 +5,81 @@
 
 class Spring {
 public:
-	Spring(XGLVertex p1, XGLVertex p2) : p1(p1), p2(p2) {};
+	Spring(XGLShape* s1, XGLShape* s2) : s1(s1), s2(s2) {};
 
 	void Integrate() {
-		// get current displacement of ball2 from resting length of spring 
-		float d = p2.x - (p1.x + length);
+		// get normalize direction vectors between the 2 shapes
+		d1 = glm::normalize(s2->p - s1->p);
+		d2 = glm::normalize(s1->p - s2->p);
+
+		// get current displacement between shapes 
+		float displacement = glm::distance(s2->p,s1->p);
+
+		// adjust displacement against length of spring
+		displacement -= length;
 
 		// calculate spring force: displacement * mass
-		f = k * d * m;
+		float force = k * displacement * m;
 
-		// add friction force (damping)
-		f -= v * 0.01;
+		// substrace friction force (damping)
+		force -= velocity * friction;
 
-		// accumulate calculated force in velocity
-		v += f;
+		// accumulate force in velocity
+		velocity += force;
 
-		p2.x -= v * 0.01;
+		// move shape 2 toward shape 1
+		s2->p += d2 * (velocity * timeStep);
+		// move shape 1 toward shape 2
+		s1->p += d1 * (velocity * timeStep);
+
+		// update their model matrices from their positions
+		s1->model = glm::translate(glm::mat4(), s1->p);
+		s2->model = glm::translate(glm::mat4(), s2->p);
 	}
-
-	XGLVertex P2() { return p2; };
-
 private:
-	XGLVertex p1, p2;
+	XGLShape *s1, *s2;
+	XGLVertex d1,d2;
 
-	// resting length;
-	float length { 10.0f };
-	float k{ 0.1f };
-	float f{};
-	float m{ 4 };
-	float v{};
+	// resting length, spring coefficient, damping, mass of end points
+	float length{ 10.0f }, k{ 0.1f }, friction{ 0.025f }, m{ 4 }, velocity{ 0.0f }, timeStep{ 1.0f / 60.0f };
 };
 
-Spring *spring;
+Spring *spring1, *spring2, *spring3;
+Spring *spring4, *spring5, *spring6;
 
 void ExampleXGL::BuildScene() {
-	XGLSphere *ball1, *ball2;
+	XGLSphere *ball1, *ball2, *ball3, *ball4;
 
 	AddShape("shaders/specular", [&](){ ball1 = new XGLSphere(1.0f, 32); return ball1; });
 	ball1->SetName("Ball");
-	ball1->model = glm::translate(glm::mat4(), glm::vec3(-10, 0, 0));
 	ball1->p = { -10.0f, 0.0f, 0.0f };
 
 	AddShape("shaders/specular", [&](){ ball2 = new XGLSphere(1.0f, 32); return ball2; });
 	ball2->SetName("Ball");
-	ball2->model = glm::translate(glm::mat4(), glm::vec3(10, 0, 0));
-	ball2->p = { 10.0f, 0.0f, 0.0f };
+	ball2->p = { 10.0f, 0.0f, 10.0f };
 
-	spring = new Spring(ball1->p, ball2->p);
+	AddShape("shaders/specular", [&](){ ball3 = new XGLSphere(1.0f, 32); return ball3; });
+	ball3->SetName("Ball");
+	ball3->p = { 0.0f, -5.0f, 0.0f };
 
-	ball2->SetAnimationFunction([ball2](float clock) {
-		spring->Integrate();
-		ball2->model = glm::translate(glm::mat4(), spring->P2());
+	AddShape("shaders/specular", [&](){ ball4 = new XGLSphere(1.0f, 32); return ball4; });
+	ball4->SetName("Ball");
+	ball4->p = { 0.0f, 0.0f, 10.0f };
+
+	spring1 = new Spring(ball1, ball2);
+	spring2 = new Spring(ball2, ball3);
+	spring3 = new Spring(ball3, ball1);
+
+	spring4 = new Spring(ball1, ball4);
+	spring5 = new Spring(ball2, ball4);
+	spring6 = new Spring(ball3, ball4);
+
+	AddPreRenderFunction([](float clock) {
+		spring1->Integrate(); 
+		spring2->Integrate();
+		spring3->Integrate();
+		spring4->Integrate();
+		spring5->Integrate();
+		spring6->Integrate();
 	});
 }
