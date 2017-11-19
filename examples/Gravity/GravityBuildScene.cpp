a55b1d1f5985b916cc7ad16873ddf80a680af0a6
Evan Mortimore <evan-git@madstyle.net>
2017-11-18 23:27:27 -0800

    Added bouncing ball to GravityBuildScene.cpp

--- examples/Gravity/GravityBuildScene.cpp
+++ examples/Gravity/GravityBuildScene.cpp
@@ -12,10 +12,30 @@
 #include "ExampleXGL.h"
 #include "xphybody.h"
 
+const glm::vec3 g = { 0.0f, 0.0f, -9.80665f };
+XGLSphere *shape;
+
 void ExampleXGL::BuildScene() {
-	XGLShape *shape;
 
 	AddShape("shaders/specular", [&](){ shape = new XGLSphere(0.5f, 64); return shape; });
 	shape->p = { 0.0f, 0.0f, 10.0f };
+	shape->m = 1.0f;
 	shape->SetMatrix();
+
+	// A bouncing ball simulation, with damping
+	shape->SetAnimationFunction([&](float clock){
+		XPhyBody *pb = static_cast<XPhyBody *>(shape);
+
+		if (pb->p.z <= shape->radius) {
+			pb->v *= -0.6f;				// semi elastic bounce
+			pb->p.z = shape->radius;	// fix overshoot	
+		}
+		else
+			pb->v += (g / 600.0f);		// integrate velocity
+
+		pb->p += pb->v;
+
+		// set model matrix (affect the visuals) with the result
+		pb->SetMatrix();
+	});
 }
