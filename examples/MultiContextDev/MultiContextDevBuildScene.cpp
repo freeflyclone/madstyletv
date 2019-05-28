dcded9a1d1d989dc6d9dbc319c289bddf166c017
Evan Mortimore <evan-git@madstyle.net>
2019-05-27 17:22:59 -0700

    MultiContext clean-up and comments, no functional changes.

--- examples/MultiContextDev/MultiContextDevBuildScene.cpp
+++ examples/MultiContextDev/MultiContextDevBuildScene.cpp
@@ -8,8 +8,8 @@
 ** multi-threaded OpenGL goodness.
 **
 ** Start with a background thread / GL context that repeatedly 
-** copies alternating frames to a PBO to see what opengl debug
-** output has to say about buffer usage.
+** copies alternating frames to an oversized PBO to see what 
+** opengl debug output has to say about buffer usage.
 **
 ** Use XTimer to measure transfer to PBO and and transfer from
 ** PBO (via glTexSubImage2D())
@@ -23,13 +23,17 @@ class XGLContext : public XThread {
 public:
 	XGLContext(ExampleXGL* pxgl, int w, int h, int c) : pXgl(pxgl), width(w), height(h), channels(c), XThread("XGLContextThread") {
 		glfwSetErrorCallback(ErrorFunc);
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 
+		// need 2nd OpenGL context, which GLFW creates with glfwCreateWindow()
+		// hint to GLFW that the window is not visible, and make it small to save memory
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 		if ((mWindow = glfwCreateWindow(32, 32, "Offscreen", NULL, pXgl->window)) == nullptr)
 			xprintf("Oops, glfwCreateWindow() failed\n");
 
+		// make new OpenGL context current so we can set it up.
 		glfwMakeContextCurrent(mWindow);
 
+		// calculate the size of one image
 		pboSize = width*height*channels;
 
 		glGenBuffers(1, &pboId);
@@ -37,6 +41,7 @@ public:
 
 		xprintf("pboId: %d\n", pboId);
 
+		// bind our PBO to UNPACK_BUFFER, allocate multiple frames, make the whole damn thing, persistently
 		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboId);
 		GL_CHECK("glBindBuffer() failed");
 
@@ -46,10 +51,10 @@ public:
 		pboBuffer = (uint8_t*)glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, pboSize*numFrames, pboFlags);
 		GL_CHECK("glMapBufferRange() failed");
 
-		if (pboBuffer == nullptr) {
+		if (pboBuffer == nullptr)
 			xprintf("Doh! glMapBufferRange() returned nullptr\n");
-		}
 
+		// restore main OpenGL context.  (it can't be bound in 2 threads at once)
 		glfwMakeContextCurrent(pXgl->window);
 
 		black = new uint8_t[pboSize];
@@ -58,7 +63,9 @@ public:
 		white = new uint8_t[pboSize];
 		memset(white, 255, pboSize);
 
+		// initialize fill and render fences
 		for (int i = 0; i < numFrames; i++) {
+			fillFences[i] = 0;
 			renderFences[i] = 0;
 		}
 	}
@@ -74,29 +81,11 @@ public:
 			int wIndex = INDEX(framesWritten);	// currently active frame for writing
 			int offset = wIndex * pboSize;		// where it is in PBO
 
-			if (renderFences[wIndex]) {
+			// make sure "renderFence" is actually valid before waiting for it
+			if (renderFences[wIndex])
 				GLenum waitRet = glClientWaitSync(renderFences[wIndex], 0, 20000000);
-				switch (waitRet) {
-				case GL_ALREADY_SIGNALED:
-					xprintf("GL_ALREADY_SIGNALED\n");
-					break;
 
-				case GL_TIMEOUT_EXPIRED:
-					xprintf("GL_TIMEOUT_EXPIRED\n");
-					break;
-
-				case GL_CONDITION_SATISFIED:
-					xprintf("GL_CONDITION_SATISFIED\n");
-					break;
-
-				case GL_WAIT_FAILED:
-					xprintf("GL_WAIT_FAILED\n");
-					break;
-
-				default:
-					xprintf("invalid wait return\n");
-				}
-			}
+			// time the copy to the PBO
 			xtimer.SinceLast();
 			if (framesWritten & 1)
 				memcpy(pboBuffer + offset, black, pboSize);
@@ -104,10 +93,12 @@ public:
 				memcpy(pboBuffer + offset, white, pboSize);
 			double et = xtimer.SinceLast();
 
+			// put a fence in so renedering thread can know if we're done with this frame
 			fillFences[INDEX(framesWritten)] = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
 			GL_CHECK("glFenceSync() failed");
 
-			double bytesPerSecond = pboSize / et;
+			// calculate transfer rate
+			//double bytesPerSecond = pboSize / et;
 			//xprintf("et: %0.8f, GB/s: %0.4f\n", et, bytesPerSecond / 1000000000.0);
 
 			std::this_thread::sleep_for(std::chrono::duration<int, std::milli>(2));
@@ -130,6 +121,8 @@ public:
 	GLsync renderFences[numFrames];
 };
 
+// derive from XGLTexQuad a class that allows us to overide it's Draw() call so
+// we can fiddle around with various asynchronous I/O transfer strategies.
 class XGLContextImage : public XGLTexQuad {
 public:
 	XGLContextImage() : XGLTexQuad(960,540,4) {
@@ -141,7 +134,7 @@ public:
 		GL_CHECK("glGenTextures() didn't work");
 	}
 
-	void SetContext(XGLContext *p) { pContext = p; }
+	void SetXGLContext(XGLContext *p) { pContext = p; }
 
 	void Draw() {
 		if (pContext->framesWritten < numFrames)
@@ -151,26 +144,31 @@ public:
 		// by the time we get here, so fiddling with the XGLContext generated
 		// PBO data won't get hosed by the default XGLTexQuad::Draw() method.
 		// So we can party on here with the async PBO here.
-
+		// Make sure the XGLContext's PBO is bound to UNPACK_BUFFER
 		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pContext->pboId);
 		GL_CHECK("glBindBuffer() didn't work.");
 
+		int rIndex = INDEX(pContext->framesRead++);		// currently active frame for reading
+
+		// wait till upload thread filling of buffer is complete.
+		glWaitSync(pContext->fillFences[rIndex], 0, GL_TIMEOUT_IGNORED);
+		GL_CHECK("glWaitSync() failed");
+
+		// initiate transfer from PBO to texture.  Yes, this makes pixel transfer synchronous with rendering.
 		xferTimer.SinceLast();
-		
-		int index = INDEX(pContext->framesRead++);
-
-		glWaitSync(pContext->fillFences[index], 0, GL_TIMEOUT_IGNORED);
-
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, ta.width, ta.height, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid *)(index*pboSize));
-
-		pContext->renderFences[index] = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
-
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, ta.width, ta.height, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid *)(rIndex*pboSize));
 		double et = xferTimer.SinceLast();
-		double bytesPerSecond = pboSize / et;
-		xprintf("xt: %0.8f, GB/s: %0.4f\n", et, bytesPerSecond / 1000000000.0);
-
 		GL_CHECK("glTexSubImage2D() failed");
 
+		// signal upload thread we're done with this frame
+		pContext->renderFences[rIndex] = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
+		GL_CHECK("glFenceSync() failed");
+
+		// calculate transfer rate
+		//double bytesPerSecond = pboSize / et;
+		//xprintf("xt: %0.8f, GB/s: %0.4f\n", et, bytesPerSecond / 1000000000.0);
+
+		// rely on base class for actual render of texture quad
 		XGLTexQuad::Draw();
 	}
 
@@ -184,12 +182,13 @@ public:
 void ExampleXGL::BuildScene() {
 	XGLContextImage *shape;
 
-	preferredSwapInterval = 1;
-
+	// get a new XGLContextImage() - currently hardcoded to 960 x 540 x 4 size
 	AddShape("shaders/tex", [&shape](){ shape = new XGLContextImage(); return shape; });
 
+	// get the data that tells us the dimensions of the texture buffer
 	XGLShape::TextureAttributes ta = shape->texAttrs[0];
 
+	// use dimensions of XGLContextImage when creating XGLContext
 	XGLContext *ac = new XGLContext(this, ta.width, ta.height, ta.channels);
 
 	// have the upright texture scaled up and made 16:9 aspect, and orbiting the origin
@@ -197,15 +196,12 @@ void ExampleXGL::BuildScene() {
 	// runs once per frame BEFORE the shape's geomentry is rendered.  A lot can
 	// be done here. Hint: scripting, physics(?)
 	shape->SetAnimationFunction([shape](float clock) {
-		float sinFunc = 0.0f;// sin(clock / 100.0f) * 10.0f;
-		float cosFunc = 0.0f;// cos(clock / 100.0f) * 10.0f;
-		glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(cosFunc, sinFunc, 5.4f));
-
+		glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0, 0, 5.4f));
 		glm::mat4 scale = glm::scale(glm::mat4(), glm::vec3(9.6f, 5.4f, 1.0f));
 		glm::mat4 rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 		shape->model = translate * rotate * scale;
 	});
 
-	shape->SetContext(ac);
+	shape->SetXGLContext(ac);
 	ac->Start();
 }
