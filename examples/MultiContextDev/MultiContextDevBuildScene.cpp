26f3d87fbd98615fb66078bdfc37827c37c7a667
Evan Mortimore <evan-git@madstyle.net>
2019-06-02 20:45:12 -0700

    MultiContextDev YUV working, secret sauce:...
    turned out to be setting shader uniform variables that the shader
    which texture units to use.  Found that in MP4Dev Draw() method.
    I *knew* I was missing something..

--- examples/MultiContextDev/MultiContextDevBuildScene.cpp
+++ examples/MultiContextDev/MultiContextDevBuildScene.cpp
@@ -28,6 +28,8 @@
 static const int numPlanes = 3;
 static const int numFrames = 2;
 
+uint8_t* gPboBuffer;
+
 #define INDEX(x) ((x) % numFrames)
 
 
@@ -44,6 +46,16 @@ public:
 		XGLTexQuad(w, h, c), 
 		XThread("XGLContextImageThread") 
 	{
+		// get pixel layout we expect from FFmpeg for GoPro footage
+		ppfd = new XGLPixelFormatDescriptor(AV_PIX_FMT_YUVJ420P);
+
+		// calculate the size of one image
+		chromaWidth = width >> ppfd->shiftRightW;
+		chromaHeight = height >> ppfd->shiftRightH;
+
+		AddTexture(chromaWidth, chromaHeight, 1);
+		AddTexture(chromaWidth, chromaHeight, 1);
+
 		glfwSetErrorCallback(ErrorFunc);
 
 		// need 2nd OpenGL context, which GLFW creates with glfwCreateWindow()
@@ -54,13 +66,6 @@ public:
 
 		// make new OpenGL context current so we can set it up.
 		glfwMakeContextCurrent(mWindow);
-
-		// get pixel layout we expect from FFmpeg for GoPro footage
-		ppfd = new XGLPixelFormatDescriptor(AV_PIX_FMT_YUVJ420P);
-
-		// calculate the size of one image
-		chromaWidth = width >> ppfd->shiftRightW;
-		chromaHeight = height >> ppfd->shiftRightH;
 
 		ySize = width*height*ppfd->depths[0];
 		uvSize = chromaWidth*chromaHeight*ppfd->depths[1];
@@ -80,7 +85,7 @@ public:
 		GL_CHECK("glBufferStorage() failed");
 
 		// Map the whole damn thing, persistently.
-		pboBuffer = (uint8_t*)glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, pboSize*numFrames, pboFlags);
+		gPboBuffer = pboBuffer = (uint8_t*)glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, pboSize*numFrames, pboFlags);
 		GL_CHECK("glMapBufferRange() failed");
 
 		if (pboBuffer == nullptr)
@@ -91,7 +96,6 @@ public:
 
 		// ensure a texture unit, 0th is safest
 		glActiveTexture(GL_TEXTURE0);
-
 		// get a texture buffer for each of "numFrames" * "numPlanes"
 		GLuint texId[numFrames*numPlanes];
 		glGenTextures(numFrames*numPlanes, texId);
@@ -102,6 +106,7 @@ public:
 				int h = (j == 0) ? height : chromaHeight;
 				int tIdx = i * numPlanes + j;
 
+				glActiveTexture(GL_TEXTURE0 + j);
 				glBindTexture(GL_TEXTURE_2D, texId[tIdx]);
 				glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -110,9 +115,9 @@ public:
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, w, h, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
 
-				texIds.push_back(texId[tIdx]);
-				texAttrs.push_back({ w, h, components });
-				numTextures++;
+				bgTexIds.push_back(texId[tIdx]);
+				bgTexAttrs.push_back({ w, h, components });
+				bgNumTextures++;
 			}
 		}
 		GL_CHECK("XGLContextImage::XGLContextImage(): something went wrong with texture allocation");
@@ -120,13 +125,17 @@ public:
 		ta = texAttrs[0];
 		xprintf("imageSize: %d,%d,%d\n", ta.width, ta.height, ta.channels);
 
-		// init a black image buffer
+		// init a black image buffer (YUV420)
 		black = new uint8_t[pboSize];
 		memset(black, 0, ySize);
+		memset(black + ySize, 127, uvSize);
+		memset(black + ySize + uvSize, 127, uvSize);
 
-		// init a white image buffer
+		// init a white image buffer (YUV420)
 		white = new uint8_t[pboSize];
 		memset(white, 255, ySize);
+		memset(white + ySize, 127, uvSize);
+		memset(white + ySize + uvSize, 127, uvSize);
 
 		// initialize fill and render fences
 		for (int i = 0; i < numFrames; i++) {
@@ -153,17 +162,24 @@ public:
 
 			// simulate what an ffmpeg decoder thread would do per frame
 			if (framesWritten & 1) {
-				memcpy(pboBuffer + offset, black, ySize);
+				memcpy(pboBuffer + offset, black, pboSize);
 			}
 			else {
-				memcpy(pboBuffer + offset, white, ySize);
+				memcpy(pboBuffer + offset, white, pboSize);
 			}
 
-			glBindTexture(GL_TEXTURE_2D, texIds[wIndex*numPlanes]);
-			GL_CHECK("glBindTexture() failed");
-
-			// Initiate a DMA from the PBO to the texture memory in the new context
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, bgTexIds[wIndex*numPlanes]);
 			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)(wIndex*pboSize));
+
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, bgTexIds[wIndex*numPlanes + 1]);
+			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, chromaWidth, chromaHeight, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)(wIndex*pboSize+ySize));
+
+			glActiveTexture(GL_TEXTURE2);
+			glBindTexture(GL_TEXTURE_2D, bgTexIds[wIndex*numPlanes + 2]);
+			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, chromaWidth, chromaHeight, GL_RED, GL_UNSIGNED_BYTE, (GLvoid *)(wIndex*pboSize + ySize + uvSize));
+
 			GL_CHECK("glTexSubImage() failed");
 
 			// put a fence in so renedering context can know if we're done with this frame
@@ -181,13 +197,28 @@ public:
 
 		int rIndex = INDEX(framesRead++);		// currently active frame for reading
 
+		glProgramUniform1i(shader->programId, glGetUniformLocation(shader->programId, "texUnit0"), 0);
+		glProgramUniform1i(shader->programId, glGetUniformLocation(shader->programId, "texUnit1"), 1);
+		glProgramUniform1i(shader->programId, glGetUniformLocation(shader->programId, "texUnit2"), 2);
+
 		// wait till upload thread filling of buffer is complete.
 		glWaitSync(fillFences[rIndex], 0, GL_TIMEOUT_IGNORED);
 		GL_CHECK("glWaitSync() failed");
 
 		// True asynchronous texture upload... just bind the relevant texture
 		// that was uploaded by the upload thread of XGLContext
-		glBindTexture(GL_TEXTURE_2D, texIds[rIndex]);
+		// Y
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, bgTexIds[rIndex*numPlanes]);
+
+		// U
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, bgTexIds[rIndex*numPlanes + 1]);
+
+		// V
+		glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, bgTexIds[rIndex*numPlanes + 2]);
+
 		GL_CHECK("glBindTexture() failed");
 
 		// signal upload thread we're done with this frame
@@ -229,8 +260,9 @@ public:
 	GLsync renderFences[numFrames];
 
 	// texture management stuff
-	std::vector<GLuint> texIds;
-	GLuint numTextures{ 0 };
+	std::vector<GLuint> bgTexIds;
+	XGLBuffer::TextureAttributesList bgTexAttrs;
+	GLuint bgNumTextures{ 0 };
 
 	XGLPixelFormatDescriptor* ppfd{ nullptr };
 	int ySize{ 0 }, uvSize{ 0 };
