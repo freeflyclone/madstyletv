d36b7a3a7191a5d719f18f548cc60306843b0547
Evan Mortimore <evan-git@madstyle.net>
2019-07-14 16:45:11 -0700

    Added ImGui dockable windows...
    ... which was the whole point of upgrading to glfw 3.4

--- examples/ImGuiDev/ImGuiDevBuildScene.cpp
+++ examples/ImGuiDev/ImGuiDevBuildScene.cpp
@@ -22,6 +22,9 @@ public:
 		ImGui::CreateContext();
 
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		pImGuiIO = &io;
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 
 		ImGui::StyleColorsDark();
 
@@ -42,11 +45,23 @@ public:
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// Update and Render additional Platform Windows
+		// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+		//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+		if (pImGuiIO->ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
 	}
 
 private:
 	XGL* pXgl{ nullptr };
 	bool show_demo_window = true;
+	ImGuiIO* pImGuiIO{ nullptr };
 };
 
 void ExampleXGL::BuildScene() {
