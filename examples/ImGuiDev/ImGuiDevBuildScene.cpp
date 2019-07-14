ff04006b4afcf487c9cddb63bb7a757b902c0b6c
Evan Mortimore <evan-git@madstyle.net>
2019-07-14 14:25:26 -0700

    Success: Dear ImGUI integration first light!...
    ...Dear ImGUI as an XGLShape object is working, with "zzz" shader.
    Using "zzz" is a kludge/hack to get XGLImGui to render last, thus on top
    of all other XGLShape objects. Sufficient for first pass.

--- examples/ImGuiDev/ImGuiDevBuildScene.cpp
+++ examples/ImGuiDev/ImGuiDevBuildScene.cpp
@@ -8,11 +8,52 @@
 ** UI experiences, which I care about.
 **************************************************************/
 #include "ExampleXGL.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 bool guiIsActive{ false };
+const char* glsl_version = "#version 150";
+
+class XGLImGui : public XGLShape {
+public:
+	XGLImGui(ExampleXGL* pXgl) : pXgl(pXgl) {
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+		ImGui::StyleColorsDark();
+
+		ImGui_ImplGlfw_InitForOpenGL(pXgl->window, true);
+		ImGui_ImplOpenGL3_Init(glsl_version);
+
+		ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\Arial.ttf", 18.0f);
+		IM_ASSERT(font != NULL);
+	};
+
+	void Draw() {
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	}
+
+private:
+	XGL* pXgl{ nullptr };
+	bool show_demo_window = true;
+};
 
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
+	XGLImGui *pImGui;
+
+	AddShape("shaders/zzz", [&](){ pImGui = new XGLImGui(this); return pImGui; });
 
 	AddShape("shaders/000-simple", [&](){ shape = new XGLTriangle(); return shape; });
 }
