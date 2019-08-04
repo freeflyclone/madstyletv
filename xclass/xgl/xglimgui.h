fee0f30f05e94a20dbf55bab4bf8bf74b94e224d
Evan Mortimore <evan-git@madstyle.net>
2019-08-04 09:36:25 -0700

    Separated XGLImGui class into it's own file...
    ..staging for xclass.lib in the future.

--- xclass/xgl/xglimgui.h
+++ xclass/xgl/xglimgui.h
@@ -5,4 +5,23 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+class XGLImGui : public XGLTexQuad {
+public:
+	XGLImGui();
+	void Draw();
+	void SetMadStyleTheme();
+
+private:
+	bool demoWindow{ true };
+
+	// the Icon files are arranged in a 14 x 15 grid.
+	const float dX{ 1.0f / 14.0f };
+	const float dY{ 1.0f / 15.0f };
+	int iconX{ 0 };
+	int iconY{ 0 };
+	ImVec4 iconTint{ 0.75, 0.75, 0.75, 1.0 };
+	int iconPreviewSize{ 4 };
+};
+
+
 #endif
