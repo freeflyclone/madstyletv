c851e7c297114da047598aeac5d732e54d0b3536
Evan Mortimore <evan-git@madstyle.net>
2019-08-17 19:42:55 -0700

    FreeTypeDev2: indicatorIndex w/tail to right side of glyph bounding box.

--- examples/FreeTypeDev2/FreeTypeDev2BuildScene.cpp
+++ examples/FreeTypeDev2/FreeTypeDev2BuildScene.cpp
@@ -109,12 +109,37 @@ public:
 			AdvanceGlyphPosition();
 		}
 
+		bb = Outline()[0].bb;
+		bb.ul.x /= scaleFactor;
+		bb.ul.y /= scaleFactor;
+		bb.lr.x /= scaleFactor;
+		bb.lr.y /= scaleFactor;
+
+		// add a couple of vertices at the end of the XGLVertexList
+		// Used for drawing a ray from current indicatorIdx to right side of
+		// bounding box for the glyph.
+		v.push_back({ {0.0, 0.0, 0.0}, {}, {}, XGLColors::white });
+		v.push_back({ {1.0, 1.0, 0.0}, {}, {}, XGLColors::white });
+
 		// update this shape's VBO with new geometry from the CPU-side XGLVertexList
 		// so it will actually be seen.
 		Load(shader, v, idx);
 	}
 
 	void Draw() {
+		int endIndex = contourOffsets[contourOffsets.size() - 1];
+		XGLVertexAttributes *pData = v.data();
+		int length = 2 * sizeof(XGLVertexAttributes);
+		int endOffset = endIndex * sizeof(XGLVertexAttributes);
+
+		if (showTail) {
+			v[endIndex].v.x = v[indicatorIdx].v.x;
+			v[endIndex].v.y = v[indicatorIdx].v.y;
+			v[endIndex + 1].v.x = bb.lr.x;
+			v[endIndex + 1].v.y = v[indicatorIdx].v.y;
+			glBufferSubData(GL_ARRAY_BUFFER, endOffset, length, &pData[endIndex]);
+		}
+
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -130,11 +155,16 @@ public:
 			glPointSize(8.0);
 			glDrawArrays(GL_POINTS, indicatorIdx, 1);
 			glPointSize(1.0);
+
+			if (showTail)
+				glDrawArrays(GL_LINES, contourOffsets[contourOffsets.size() - 1], 2);
 		}
 		glDisable(GL_BLEND);
 	}
 
 	int indicatorIdx{ 0 };
+	bool showTail = false;
+	FT::BoundingBox bb;
 
 private:
 	// These 2 numbers help Freetype math have adequate precision...
@@ -167,13 +197,9 @@ void ExampleXGL::BuildScene() {
 	if ((ig = (XGLImGui *)(GetGuiManager()->FindObject("TitlerGui"))) != nullptr) {
 		ig->AddMenuFunc([&]() {
 			if (ImGui::Begin("Titler")) {
-				if (ImGui::CollapsingHeader("Controls")) {
+				if (ImGui::CollapsingHeader("Tweeks")) {
 					ImGui::SliderInt("Indicator Index", &pFt->indicatorIdx, 0, pFt->v.size()-1);
-					/*
-					ImGui::SliderInt("Row", &iconY, 0, 14);
-					ImGui::ColorEdit4("Tint", &iconTint.x);
-					ImGui::SliderInt("Size/32)", &iconPreviewSize, 1, 8);
-					*/
+					ImGui::Checkbox("Show Tail", &pFt->showTail);
 				}
 				ImGui::End();
 			}
