fee6c9ce3d9effecd59ae00b7011d0da79021c72
Evan Mortimore <evan-git@madstyle.net>
2019-09-02 15:38:53 -0700

    Add BezierTest example.

---
+++ examples/BezierTest/BezierTestBuildScene.cpp
@@ -0,0 +1,43 @@
+/**************************************************************
+** BezierTestBuildScene.cpp
+**
+** Just to demonstrate instantiation of a "ground"
+** plane and a single triangle, with default camera manipulation
+** via keyboard and mouse.
+**************************************************************/
+#include "ExampleXGL.h"
+
+class XGLNewTriangle : public XGLShape {
+public:
+	XGLNewTriangle() {
+		SetName("XGLNewTriangle");
+		v.push_back({ { -1, 0, 0 }, {}, {}, XGLColors::red });
+		v.push_back({ { 1, 0, 0 }, {}, {}, XGLColors::red });
+		v.push_back({ { 0, 1.412, 0 }, {}, {}, {0, 0, 1, -1} });
+	}
+
+	void Draw(){
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		GL_CHECK("glDrawArrays() failed");
+	};
+};
+
+void ExampleXGL::BuildScene() {
+	XGLShape *shape;
+	XGLVertex triangle[3];
+
+	AddShape("shaders/bezier", [&](){ shape = new XGLNewTriangle(); return shape; });
+
+	for (int i = 0; i < 3; i++)
+		triangle[i] = shape->v[i].v;
+
+	// shader must be coded such that "triangle" uniform doesn't get optimized out by the shader compiler
+	// ie: it has to be used somehow.
+	GLint location = glGetUniformLocation(shape->shader->programId, "triangle");
+	GL_CHECK("glGetUniformLocation() failed");
+	if (location != -1) {
+		xprintf("Found uniform \"triangle\"\n");
+		glUniform3fv(location, 3, glm::value_ptr(triangle[0]));
+		GL_CHECK("glUniform3fv() failed");
+	}
+}
---
+++ shaders/bezier.frag
@@ -0,0 +1,32 @@
+#version 150
+
+precision highp float;
+
+uniform vec3 cameraPosition;
+
+layout (std140) uniform LightData {
+   vec4 position;
+   vec4 intensities; //a.k.a the color of the light
+   float attenuation;
+   float ambientCoefficient;
+} light;
+
+uniform float materialShininess;
+uniform vec3 materialSpecularColor;
+uniform vec3 triangle[3];
+in  vec4 ex_Color;
+
+out vec4 out_Color;
+
+float distance(vec4 p) {
+	vec3 p0 = triangle[0];
+	vec3 p1 = triangle[1];
+	vec3 c = triangle[2];
+
+	return -triangle[0].x * triangle[1].x;
+}
+
+void main(void)
+{
+    out_Color = ex_Color * distance(gl_FragCoord);
+}
