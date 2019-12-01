777d9301d3114caf06b52c7dbd9532923d95eec0
Evan Mortimore <evan-git@madstyle.net>
2019-12-01 14:49:02 -0800

    First display of STL model using Assimp.

--- examples/AssimpDev/AssimpDevBuildScene.cpp
+++ examples/AssimpDev/AssimpDevBuildScene.cpp
@@ -1,25 +1,77 @@
 /**************************************************************
 ** AssimpDevBuildScene.cpp
 **
-** Just to demonstrate instantiation of a "ground"
-** plane and a single triangle, with default camera manipulation
-** via keyboard and mouse.
+** Demonstrate the Assimp library.
 **************************************************************/
 #include "ExampleXGL.h"
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-void ExampleXGL::BuildScene() {
-	XGLShape *shape;
-	Assimp::Importer importer;
+class XGLAssimp : public XGLShape
+{
+public:
+	XGLAssimp(const std::string fileName)
+	{
+		Assimp::Importer importer;
+
+		const aiScene* scene = importer.ReadFile(fileName, 0);
+		if (scene == nullptr)
+		{
+			xprintf("Failed to load file '%s'\n", fileName.c_str());
+			return;
+		}
+
+		ConvertScene(scene);
+	}
+
+	void ConvertScene(const aiScene* scene)
+	{
+		for (int meshIndex = 0; meshIndex < scene->mNumMeshes; meshIndex++)
+		{
+			aiMesh* mesh = scene->mMeshes[meshIndex];
+			ConvertMesh(mesh);
+		}
+	}
+
+	void ConvertMesh(aiMesh* mesh)
+	{
+		for (int index = 0; index < mesh->mNumVertices; index++)
+		{
+			aiVector3D vrtx = mesh->mVertices[index];
+
+			v.push_back
+			(
+				{ 
+					{ vrtx.x, vrtx.y, vrtx.z }, 
+					{}, 
+					{}, 
+					XGLColors::yellow 
+				}
+			);
+		}
+	}
+
+	void Draw()
+	{
+		glPointSize(4.0f);
+		glDrawArrays(GL_POINTS, 0, GLsizei(v.size()));
+		GL_CHECK("glDrawArrays() failed");
+	}
+};
+
+void ExampleXGL::BuildScene() 
+{
+	XGLAssimp *shape;
+
+	std::string shapePath = pathToAssets + "/assets/3DModels/motor-mount-3.stl";
 
 	AddShape
 	(
 		"shaders/000-simple", 
 		[&]() 
 		{ 
-			shape = new XGLTriangle(); return shape; 
+			shape = new XGLAssimp(shapePath); return shape; 
 		}
 	);
 }
