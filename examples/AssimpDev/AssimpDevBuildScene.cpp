094952a4886ae972fd58d7c78153a706cb2cf3a1
Evan Mortimore <evan-git@madstyle.net>
2019-12-01 19:22:57 -0800

    Add normals & faces to AssimpDev demo.

--- examples/AssimpDev/AssimpDevBuildScene.cpp
+++ examples/AssimpDev/AssimpDevBuildScene.cpp
@@ -15,7 +15,13 @@ public:
 	{
 		Assimp::Importer importer;
 
-		const aiScene* scene = importer.ReadFile(fileName, 0);
+		const aiScene* scene;
+		scene = importer.ReadFile
+		(
+			fileName,
+			aiProcess_Triangulate | aiProcess_SortByPType
+		);
+
 		if (scene == nullptr)
 		{
 			xprintf("Failed to load file '%s'\n", fileName.c_str());
@@ -30,33 +36,49 @@ public:
 		for (int meshIndex = 0; meshIndex < scene->mNumMeshes; meshIndex++)
 		{
 			aiMesh* mesh = scene->mMeshes[meshIndex];
-			ConvertMesh(mesh);
+			ConvertMeshToXglVertexList(mesh);
+			ConvertMeshToXglFaces(mesh);
 		}
 	}
 
-	void ConvertMesh(aiMesh* mesh)
+	void ConvertMeshToXglVertexList(aiMesh* mesh)
 	{
 		for (int index = 0; index < mesh->mNumVertices; index++)
 		{
 			aiVector3D vrtx = mesh->mVertices[index];
+			aiVector3D n = mesh->mNormals[index];
+
+			// normals from model *might* not be unit length
+			// according to Assimp docs.
+			XGLVertex nrml = glm::normalize(glm::vec3(n.x,n.y,n.z));
 
 			v.push_back
 			(
 				{ 
 					{ vrtx.x, vrtx.y, vrtx.z }, 
 					{}, 
-					{}, 
+					nrml,
 					XGLColors::yellow 
 				}
 			);
 		}
 	}
 
+	void ConvertMeshToXglFaces(aiMesh* mesh)
+	{
+		for (int index = 0; index < mesh->mNumFaces; index++)
+		{
+			aiFace face = mesh->mFaces[index];
+
+			for (int j = 0; j < face.mNumIndices; j++)
+				idx.push_back(face.mIndices[j]);
+		}
+	}
+
 	void Draw()
 	{
-		glPointSize(4.0f);
-		glDrawArrays(GL_POINTS, 0, GLsizei(v.size()));
-		GL_CHECK("glDrawArrays() failed");
+		glDrawElements(GL_TRIANGLES, (GLsizei)idx.size(), XGLIndexType, 0);
+		GL_CHECK("glDrawElements() failed");
 	}
 };
 
@@ -64,14 +86,13 @@ void ExampleXGL::BuildScene()
 {
 	XGLAssimp *shape;
 
-	std::string shapePath = pathToAssets + "/assets/3DModels/motor-mount-3.stl";
+	std::string shapePath = pathToAssets + "/assets/3DModels/mm5.stl";
 
 	AddShape
 	(
-		"shaders/000-simple", 
-		[&]() 
-		{ 
-			shape = new XGLAssimp(shapePath); return shape; 
-		}
+		"shaders/specular", 
+		[&]() {	shape = new XGLAssimp(shapePath); return shape;	}
 	);
+
+	shape->attributes.diffuseColor = XGLColors::yellow;
 }
