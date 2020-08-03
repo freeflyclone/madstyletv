172ebbc0a9ed27431837ccfd75b330eb6711b970
Evan Mortimore <evan-github@madstyle.net>
2020-08-02 20:01:57 -0700

    Fix bug in get_ffmpeg.bat. It actually works now.

--- xclass/3rdParty/ffmpeg/get_ffmpeg.bat
+++ xclass/3rdParty/ffmpeg/get_ffmpeg.bat
@@ -3,14 +3,18 @@ rem unless you have curl & tar in your path
 rem ------------------------------------------
 cd %1
 if not exist ffmpeg.zip (
-	echo "Downloading ffmpeg.zip from libs.madstyle.tv"
+	echo Downloading ffmpeg.zip from libs.madstyle.tv
 	curl http://libs.madstyle.tv/libffmpeg_4.4.r98605_msvc15_x64.zip --output ffmpeg.zip
 )
 
 if not exist lib (
-	echo "lib folder not found, expanding ffmpeg.zip"
+	echo lib folder not found, expanding ffmpeg.zip
 	tar xf ffmpeg.zip
+) else (
+	echo ffmpeg lib folder found, nothing to do
 )
-else (
-	echo "ffmpeg lib folder found, nothing to do"
+
+if not exist ..\..\..\bin\avcodec.dll (
+	copy bin\x64\*.dll ..\..\..\bin
 )
+
