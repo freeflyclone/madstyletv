ccb02386d8aa3ad2f9dd8b4009d813ed4e21a02b
Evan Mortimore <evan-github@madstyle.net>
2020-08-02 19:40:37 -0700

    Added Target in 3rdParty.proj to extract ffmpeg binaries with get_ffmpeg.bat

--- xclass/3rdParty/ffmpeg/get_ffmpeg.bat
+++ xclass/3rdParty/ffmpeg/get_ffmpeg.bat
@@ -1,6 +1,16 @@
 rem This will likely only work in Windows 10, 
 rem unless you have curl & tar in your path
 rem ------------------------------------------
-curl http://libs.madstyle.tv/libffmpeg_4.4.r98605_msvc15_x64.zip --output ffmpeg.zip
-tar xf ffmpeg.zip
-del ffmpeg.zip
+cd %1
+if not exist ffmpeg.zip (
+	echo "Downloading ffmpeg.zip from libs.madstyle.tv"
+	curl http://libs.madstyle.tv/libffmpeg_4.4.r98605_msvc15_x64.zip --output ffmpeg.zip
+)
+
+if not exist lib (
+	echo "lib folder not found, expanding ffmpeg.zip"
+	tar xf ffmpeg.zip
+)
+else (
+	echo "ffmpeg lib folder found, nothing to do"
+)
