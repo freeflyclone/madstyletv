84ab20062298f187cd60b27aa65d1c418ec6b036
Evan Mortimore <evan-github@madstyle.net>
2021-02-12 20:03:44 -0800

    First round of clean up: replaced AllignedMalloc() with C++(11?) equivalent "new" expression.

--- examples/R3DDev/R3DDevBuildScene.cpp
+++ examples/R3DDev/R3DDevBuildScene.cpp
@@ -1,38 +1,13 @@
 /**************************************************************
 ** R3DDevBuildScene.cpp
 **
-** Just to demonstrate instantiation of a "ground"
-** plane and a single triangle, with default camera manipulation
-** via keyboard and mouse.
+** Mission: Integrate R3DSDK fully, including CUDA
 **************************************************************/
 #include "ExampleXGL.h"
 #include <cstdlib>
 
 #include "R3DSDK.h"
 static const char* clipName = "BMX.RDC/A006_A050_0808WW_001.R3D";
-
-unsigned char * AlignedMalloc(size_t & sizeNeeded)
-{
-	// alloc 15 bytes more to make sure we can align the buffer in case it isn't
-	unsigned char * buffer = (unsigned char *)malloc(sizeNeeded + 15U);
-
-	if (!buffer)
-		return NULL;
-
-	sizeNeeded = 0U;
-
-	// cast to a 32-bit or 64-bit (depending on platform) integer so we can do the math
-	uintptr_t ptr = (uintptr_t)buffer;
-
-	// check if it's already aligned, if it is we're done
-	if ((ptr % 16U) == 0U)
-		return buffer;
-
-	// calculate how many bytes we need
-	sizeNeeded = 16U - (ptr % 16U);
-
-	return buffer + sizeNeeded;
-}
 
 void ExampleXGL::BuildScene() {
 	using namespace R3DSDK;
@@ -63,16 +38,13 @@ void ExampleXGL::BuildScene() {
 	xprintf("Loaded %s\n", clipName);
 
 	size_t width = clip->Width();
-	size_t height = clip->Height();	// going to do a half resolution decode
+	size_t height = clip->Height();
 
 	// three channels (RGB) in 16-bit (2 bytes) requires this much memory:
 	size_t memNeeded = width * height * 3U * 2U;
-
-	// make a copy for AlignedMalloc (it will change it)
-	size_t adjusted = memNeeded;
+	size_t adjusted = memNeeded + 16;
 
 	// alloc this memory 16-byte aligned
-	//unsigned char * imgbuffer = AlignedMalloc(adjusted);
 	unsigned char *unAlignedImgBuffer = new unsigned char[adjusted];
 	unsigned char *imgbuffer = (unsigned char*)(std::align(16, memNeeded, (void*&)unAlignedImgBuffer, adjusted));
 
@@ -122,7 +94,6 @@ void ExampleXGL::BuildScene() {
 	delete clip;
 	FinalizeSdk();
 
-	//AddShape("shaders/000-simple", [&](){ shape = new XGLTriangle(); return shape; });
 	AddShape("shaders/tex", [&](){ shape = new XGLTexQuad(width,height,4,imgbuffer); return shape; });
 		glm::mat4 scale = glm::scale(glm::mat4(), glm::vec3(9.6f, 5.4f, 1.0f));
 		glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0, 0, 5.4f));
