a7f268b0ffd328dd74ddeff9550d027ca8ee7da9
Evan Mortimore <evan-github@madstyle.net>
2021-02-13 10:33:09 -0800

    Sample footage I'm starting with is 2.4:1.0 aspect ratio, changed translate z adjustment to compensate.

--- examples/R3DDev/R3DDevBuildScene.cpp
+++ examples/R3DDev/R3DDevBuildScene.cpp
@@ -144,7 +144,7 @@ void ExampleXGL::BuildScene() {
 
 	AddShape("shaders/tex16planar", [&](){ shape = new R3DPlayer(r3DClipName); return shape; });
 	glm::mat4 scale = glm::scale(glm::mat4(), glm::vec3(24.0f, 10.0f, 1.0f));
-	glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0, 0, 5.4f));
+	glm::mat4 translate = glm::translate(glm::mat4(), glm::vec3(0, 0, 10.0f));
 	glm::mat4 rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	shape->model = translate * rotate * scale;
 }
