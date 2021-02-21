1f6658f47ad54ea52f6031d13645fb9157de16e9
Evan Mortimore <evan-github@madstyle.net>
2021-02-20 17:08:25 -0800

    Add first bit of rs-depth.c sample code, and zval_in_red shader

--- examples/DepthCamDev/DepthCamDevBuildScene.cpp
+++ examples/DepthCamDev/DepthCamDevBuildScene.cpp
@@ -1,17 +1,113 @@
 /**************************************************************
 ** DepthCamDevBuildScene.cpp
 **
+** Intel's RealSense L515 LIDAR camera is some dope technology.
+** Let's see how easily we can make use of the depth data.
+** Piecemeal derive from 'rs-depth.c' sample in the RS2 SDK
 **************************************************************/
 #include "ExampleXGL.h"
+
+#include <initguid.h>
+#include <usbiodef.h>
 
 #include <librealsense2/rs.h>
 #include <librealsense2/h/rs_pipeline.h>
 #include <librealsense2/h/rs_option.h>
 #include <librealsense2/h/rs_frame.h>
+#include "example.h"
+
+#define STREAM          RS2_STREAM_DEPTH  // rs2_stream is a types of data provided by RealSense device           //
+#define FORMAT          RS2_FORMAT_Z16    // rs2_format identifies how binary data is encoded within a frame      //
+#define WIDTH           1024               // Defines the number of columns for each frame or zero for auto resolve//
+#define HEIGHT          768                 // Defines the number of lines for each frame or zero for auto resolve  //
+#define FPS             30                // Defines the rate of frames per second                                //
+#define STREAM_INDEX    0                 // Defines the stream index, used for multiple streams of the same type //
+#define HEIGHT_RATIO    20                // Defines the height ratio between the original frame to the new frame //
+#define WIDTH_RATIO     10                // Defines the width ratio between the original frame to the new frame  //
+
+// The number of meters represented by a single depth unit
+float get_depth_unit_value(const rs2_device* const dev)
+{
+	rs2_error* e = 0;
+	rs2_sensor_list* sensor_list = rs2_query_sensors(dev, &e);
+	check_error(e);
+
+	int num_of_sensors = rs2_get_sensors_count(sensor_list, &e);
+	check_error(e);
+
+	float depth_scale = 0;
+	int is_depth_sensor_found = 0;
+	int i;
+	for (i = 0; i < num_of_sensors; ++i)
+	{
+		rs2_sensor* sensor = rs2_create_sensor(sensor_list, i, &e);
+		check_error(e);
+
+		// Check if the given sensor can be extended to depth sensor interface
+		is_depth_sensor_found = rs2_is_sensor_extendable_to(sensor, RS2_EXTENSION_DEPTH_SENSOR, &e);
+		check_error(e);
+
+		if (1 == is_depth_sensor_found)
+		{
+			depth_scale = rs2_get_option((const rs2_options*)sensor, RS2_OPTION_DEPTH_UNITS, &e);
+			check_error(e);
+			rs2_delete_sensor(sensor);
+			break;
+		}
+		rs2_delete_sensor(sensor);
+	}
+	rs2_delete_sensor_list(sensor_list);
+
+	if (0 == is_depth_sensor_found)
+	{
+		printf("Depth sensor not found!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return depth_scale;
+}
+
+class XGLDepthCam : public XGLTexQuad {
+public:
+	XGLDepthCam() : XGLTexQuad() {
+		GenUShortZMap(WIDTH, HEIGHT);
+	}
+
+	void GenUShortZMap(const int width, const int height) {
+		GLuint texId;
+
+		glGenTextures(1, &texId);
+		GL_CHECK("Eh, something failed");
+		glActiveTexture(GL_TEXTURE0 + numTextures);
+		GL_CHECK("Eh, something failed");
+		glBindTexture(GL_TEXTURE_2D, texId);
+		GL_CHECK("Eh, something failed");
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		GL_CHECK("Eh, something failed");
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		GL_CHECK("Eh, something failed");
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		GL_CHECK("Eh, something failed");
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		GL_CHECK("Eh, something failed");
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		GL_CHECK("Eh, something failed");
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_SHORT, (void *)(nullptr));
+		GL_CHECK("Eh, something failed");
+
+		AddTexture(texId);
+	}
+};
 
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
+	XGLDepthCam *depthCam;
+
 	std::string imgPath = pathToAssets + "/assets/AndroidDemo.png";
+
+	AddShape("shaders/zval_in_red", [&depthCam, imgPath]() { depthCam = new XGLDepthCam(); return depthCam; });
+	depthCam->attributes.diffuseColor = XGLColors::yellow;
+/*
 	AddShape("shaders/tex", [&shape,imgPath](){ shape = new XGLTexQuad(imgPath); return shape; });
 
 	// have the upright texture scaled up and made 16:9 aspect, and orbiting the origin
@@ -26,4 +122,5 @@ void ExampleXGL::BuildScene() {
 		glm::mat4 rotate = glm::rotate(glm::mat4(), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 		shape->model = translate * rotate * scale;
 	});
+*/
 }
---
+++ shaders/zval_in_red.frag
@@ -0,0 +1,50 @@
+#version 330
+
+uniform mat4 model;
+uniform vec4 ambient;
+uniform vec4 diffuse;
+uniform vec4 specular;
+uniform float shininess;
+uniform sampler2D tex;
+
+layout (std140) uniform MatrixData {
+	mat4 projector;
+	mat4 view;
+};
+
+layout (std140) uniform LightData {
+	vec3 pos;
+	vec4 color;
+};
+
+in vec3 fragVert;
+in vec2 fragTexCoord;
+in vec3 fragNormal;
+in vec4 fragColor;
+
+out vec4 finalColor;
+
+void main() {
+    vec3 normal = normalize(fragNormal);
+    
+    //calculate the location of this fragment (pixel) in world coordinates
+    vec3 fragPosition = vec3(model * vec4(fragVert, 1));
+    
+    //calculate the vector from this pixel's surface to the light source
+    vec3 surfaceToLight = pos - fragPosition;
+
+    //calculate the cosine of the angle of incidence
+    float brightness = dot(normal, surfaceToLight) / (length(surfaceToLight) * length(normal));
+    brightness = clamp(brightness, 0, 1);
+
+    //calculate final color of the pixel, based on:
+    // 1. The angle of incidence: brightness
+    // 2. The color/intensities of the light: light.intensities
+    // 3. The texture and texture coord: texture(tex, fragTexCoord)
+    //vec4 surfaceColor = texture(tex, fragTexCoord);
+
+
+    //finalColor = (brightness * diffuse * fragColor) + (ambient * 0.1);
+    //finalColor = (brightness * diffuse) + (ambient * 0.1);
+	finalColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
