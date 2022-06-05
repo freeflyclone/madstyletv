33898fade01f754b3fdf110076619436d0c3f2b1
Evan Mortimore <evan-github@madstyle.net>
2022-06-05 13:43:21 -0700

    First hookup of actual OpenCL runtime stuff.

--- examples/OpenCLTest/OpenCLTestBuildScene.cpp
+++ examples/OpenCLTest/OpenCLTestBuildScene.cpp
@@ -7,8 +7,18 @@
 **************************************************************/
 #include "ExampleXGL.h"
 
+#include <vector>
+#include "CL/cl.hpp"
+
 void ExampleXGL::BuildScene() {
 	XGLShape *shape;
+
+	std::vector<cl::Platform> platform;
+	cl::Platform::get(&platform);
+
+	if (platform.empty()) {
+		xprintf("Doh! no OpenCL platform!\n");
+	}
 
 	AddShape("shaders/000-simple", [&](){ shape = new XGLTriangle(); return shape; });
 }
